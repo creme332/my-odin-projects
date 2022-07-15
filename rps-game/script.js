5ccef07d21d9b11d69b775160f615743f4067342
creme332 <65414576+creme332@users.noreply.github.com>
2022-07-15 18:59:55 +0400

    Add attack animation for computer and human

--- rps-game/script.js
+++ rps-game/script.js
@@ -1,12 +1,40 @@
+const PlayerCharacter = document.querySelector("#player");
+const ComputerCharacter = document.querySelector("#computer");
+
 const PlayerWeapon = document.querySelector("#player-weapon");
+const ComputerWeapon = document.querySelector("#computer-weapon");
 const audio = document.getElementById("sound");
 
+// Define animation functions 
 function playanimation(){
+    audio.currentTime = 0;
     audio.play();
-    PlayerWeapon.classList.add("animate");
+    PlayerCharacter.classList.add("animatePlayerCharacter");
+    computerPlay();
 }
+function computerPlay(){
+    ComputerCharacter.classList.add("animateComputerCharacter");
+  
+    //  Start weapon animation when player animation ends 
+    ComputerCharacter.addEventListener( "animationend",  function() {
+        ComputerCharacter.classList.remove("animateComputerCharacter");  
+        ComputerWeapon.classList.add("animateComputerWeapon");  
+} );
+}
+// Add event listeners 
 const buttons = Array.from(document.querySelectorAll(".button"));
 buttons.forEach(btn=>btn.addEventListener("click", playanimation));
-PlayerWeapon.addEventListener( "animationend",  function() {
-    PlayerWeapon.classList.remove("animate");    
+
+//  Start weapon animation when player animation ends 
+PlayerCharacter.addEventListener( "animationend",  function() {
+    PlayerCharacter.classList.remove("animatePlayerCharacter");  
+    PlayerWeapon.classList.add("animatePlayerWeapon");  
 } );
+PlayerWeapon.addEventListener( "animationend",  function() {
+    PlayerWeapon.classList.remove("animatePlayerWeapon");    
+} );
+ComputerWeapon.addEventListener( "animationend",  function() {
+    ComputerWeapon.classList.remove("animateComputerWeapon");    
+} );
+playerPlay();
+computerPlay();
