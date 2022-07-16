c5e85101777f22133207e28bdbf8ae14a771bcda
creme332 <65414576+creme332@users.noreply.github.com>
2022-07-16 15:09:44 +0400

    Add dynamic healthbar

--- rps-game/script.js
+++ rps-game/script.js
@@ -1,3 +1,66 @@
+let playerfills = document.querySelectorAll(".player-healthbar_fill");
+let PlayerHealth = 100;
+const maxPlayerHP = 100;
+const healthLoss = -10; //health points lost when a character loses.
+
+function renderPlayerHealth() {
+   var percent = PlayerHealth / maxPlayerHP * 100;
+   //Update color
+   document.documentElement.style.setProperty('--player-bar-fill', '#57e705');
+   document.documentElement.style.setProperty('--player-bar-top',  '#6aff03');
+   
+   if (percent <= 50) { //yellows
+      document.documentElement.style.setProperty('--player-bar-fill', '#d6ed20');
+      document.documentElement.style.setProperty('--player-bar-top',  '#d8ff48');   
+   }
+   if (percent <= 25) { //reds
+      document.documentElement.style.setProperty('--player-bar-fill', '#ec290a');
+      document.documentElement.style.setProperty('--player-bar-top',  '#ff3818');
+   }
+
+   playerfills.forEach(fill => {
+      fill.style.width = percent+"%";
+   })   
+}
+
+function updatePlayerHealth() {
+   PlayerHealth += healthLoss;
+   PlayerHealth = PlayerHealth > maxPlayerHP ? maxPlayerHP : PlayerHealth;
+   PlayerHealth = PlayerHealth < 0 ? 0 : PlayerHealth;
+   renderPlayerHealth();
+}
+
+
+let ComputerHealth = 100;
+const maxComputerHP = 100;
+let computerfills = document.querySelectorAll(".computer-healthbar_fill");
+function renderComputerHealth() {
+   var percent = ComputerHealth / maxComputerHP * 100;
+   //Update color
+   document.documentElement.style.setProperty('--computer-bar-fill', '#57e705');
+   document.documentElement.style.setProperty('--computer-bar-top',  '#6aff03');
+   
+   if (percent <= 50) { //yellows
+      document.documentElement.style.setProperty('--computer-bar-fill', '#d6ed20');
+      document.documentElement.style.setProperty('--computer-bar-top',  '#d8ff48');   
+   }
+   if (percent <= 25) { //reds
+      document.documentElement.style.setProperty('--computer-bar-fill', '#ec290a');
+      document.documentElement.style.setProperty('--computer-bar-top',  '#ff3818');
+   }
+
+   computerfills.forEach(fill => {
+      fill.style.width = percent+"%";
+   })   
+}
+
+function updateComputerHealth() {
+    ComputerHealth += healthLoss;
+    ComputerHealth = ComputerHealth > maxComputerHP ? maxComputerHP : ComputerHealth;
+    ComputerHealth = ComputerHealth < 0 ? 0 : ComputerHealth;
+   renderComputerHealth();
+}
+
 const PlayerCharacter = document.querySelector("#player");
 const ComputerCharacter = document.querySelector("#computer");
 
@@ -10,16 +73,26 @@ function computerChoice(){
     let index = Math.floor(Math.random() * 3) //generate indice 0-2
     return choices[index];
 }
-// Define animation functions 
+function winner(playermove, computermove){
+    if(playermove==computermove)return "draw";
+    const LoseTo = {"rock":"paper","paper":"scissors","scissors":"rockk"};
+    if(playermove == LoseTo[computermove])return "player";
+    return "computer";
+}
+
 function Battle(e){
     let PlayerPressed = e.target.id;
-    let computerPressed = computerChoice();
+    // let computerPressed = computerChoice();
+    let computerPressed = "rock";
     let playerWeaponImgClass = PlayerPressed + "-background";
     let computerWeaponImgClass = computerPressed + "-background";
 
+    let roundwinner = winner(PlayerPressed, computerPressed);
+    console.log(roundwinner);
+
     console.log(playerWeaponImgClass, computerWeaponImgClass);
     audio.currentTime = 0;
-    audio.play();
+    audio.play(); //play button sound
 
     //display character animations
     PlayerCharacter.classList.add("animatePlayerCharacter");
@@ -38,18 +111,25 @@ function Battle(e){
         ComputerWeapon.classList.add("animateComputerWeapon"); 
     } );
 
-        // remove weapon animation class for all weapons after weapon animation ends.
-        PlayerWeapon.addEventListener( "animationend",  function() {
-            PlayerWeapon.classList.remove("animatePlayerWeapon"); 
-            PlayerWeapon.classList.remove(playerWeaponImgClass); 
-    
-        } );
-        ComputerWeapon.addEventListener( "animationend",  function() {
-            ComputerWeapon.classList.remove("animateComputerWeapon"); 
-            ComputerWeapon.classList.remove(computerWeaponImgClass);  
-        } );
+    // remove weapon animation class for all weapons after weapon animation ends.
+    PlayerWeapon.addEventListener( "animationend",  function() {
+        PlayerWeapon.classList.remove("animatePlayerWeapon"); 
+        PlayerWeapon.classList.remove(playerWeaponImgClass); 
+        if(roundwinner == "player")updateComputerHealth();
+    } );
+    ComputerWeapon.addEventListener( "animationend",  function() {
+        ComputerWeapon.classList.remove("animateComputerWeapon"); 
+        ComputerWeapon.classList.remove(computerWeaponImgClass);  
+        if(roundwinner == "computer")updatePlayerHealth();
+    } );
+
+
 }
 
-// Add event listeners 
 const buttons = Array.from(document.querySelectorAll(".button"));
 buttons.forEach(btn=>btn.addEventListener("click", Battle));
+
+
+renderPlayerHealth(0);
+renderComputerHealth(0);
+
