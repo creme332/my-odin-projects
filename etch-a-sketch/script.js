0af03a5ae163e8dd8cbe84f35cf25dbc32bc6436
creme332 <65414576+creme332@users.noreply.github.com>
2022-07-27 13:49:36 +0400

    Add bucket fill option using dfs algorithm

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,12 +1,29 @@
-let canvaStates = []; //stores colors of canvas after each change. Used for redo/undo
-let ptr = -1;
-let stateChanged = false;
-const EMPTY_COLOR = "rgb(236, 234, 234)";
+let canvaStates = []; //array of array of colors. 
+//stores colors of canvas after each change. Used for redo/undo
+let ptr = -1; //points to current displayed frame in canvaStates.
+let stateChanged = false; //was there a change to currently displayed canva?
 
-function InitialiseGrid(GRID_SIZE){
-    const canva = document.querySelector(".canva");
+const GRID_SIZE = 5; // number of cells in each row and column
+const GRID_DIMENSIONS = 350; //350px x 350px
+const canva = document.querySelector(".canva");
+
+const EMPTY_COLOR = "rgb(236, 234, 234)"; //color of empty cell
+let isDrawing = false;
+let showOutline = false;
+let pencilColor =  "red"; //initial crayon color
+let fillBucketMode = false;
+document.querySelector(".redcrayon").classList.add("selected");
+
+const toggleGridbutton = document.querySelector(".showgrid");
+const undoButton = document.querySelector(".undo");
+const redoButton = document.querySelector(".redo");
+const saveButton = document.querySelector(".save");
+const fillButton = document.querySelector(".fill");
+const crayons = document.querySelectorAll(".crayon");
+
+
+function InitialiseGrid(){
     const CELL_SIZE = 100/GRID_SIZE;
-
     let colors = []; 
 
     for (let i=0;i<GRID_SIZE*GRID_SIZE;i++){
@@ -16,52 +33,96 @@ function InitialiseGrid(GRID_SIZE){
         cell.style.width = CELL_SIZE.toString()+"%";
         cell.style.backgroundColor = EMPTY_COLOR;
         canva.appendChild(cell);
+        getCoordinates(cell);
         colors.push(EMPTY_COLOR);
     }
     canvaStates.push(colors);
     ptr++;
 }
 
+InitialiseGrid();
 
-InitialiseGrid(4);
-const toggleGridbutton = document.querySelector(".showgrid");
-const undoButton = document.querySelector(".undo");
-const redoButton = document.querySelector(".redo");
 const cells = document.querySelectorAll(".cell");
-const crayons = document.querySelectorAll(".crayon");
+
+function getCoordinates(cell){
+    //cell is a div
+    const CELL_DIMENSIONS = GRID_DIMENSIONS/GRID_SIZE;
+    console.log(CELL_DIMENSIONS);
+    let relativeX = cell.getBoundingClientRect().left - canva.getBoundingClientRect().left;
+    let relativeY = cell.getBoundingClientRect().top - canva.getBoundingClientRect().top;
+
+    // (relativeX, relativeY) : coordinates of cell in pixels relative to canva
+    let col = Math.floor(relativeX/CELL_DIMENSIONS);
+    let row = Math.floor(relativeY/CELL_DIMENSIONS);
+    cell.textContent = "("+row.toString() + ", " + col.toString()+")";
+
+    return [row,col];
+}
+function UpdateCanvaColors(updatedcolors){
+    for (let i = 0; i < cells.length; i++) {
+        if(updatedcolors[i] !=  cells[i].style.backgroundColor){
+            cells[i].style.backgroundColor = updatedcolors[i];
+        }
+    }
+}
 
 undoButton.addEventListener("click",function(){
     if (canvaStates.length!=1){
-        ptr--;
-        let updatedcolors = canvaStates[ptr];
-    
-        //change color of updated nodes
-        for (let i = 0; i < cells.length; i++) {
-            if(updatedcolors[i] !=  cells[i].style.backgroundColor){
-                cells[i].style.backgroundColor = updatedcolors[i];
-            }
-        }
+        ptr--; 
+        UpdateCanvaColors(canvaStates[ptr]);
     }
 });
 
 redoButton.addEventListener("click", function(){
     if (ptr < canvaStates.length-1){
         ptr++;
-        let updatedcolors = canvaStates[ptr];
-    
-        //change color of updated nodes
-        for (let i = 0; i < cells.length; i++) {
-            if(updatedcolors[i] !=  cells[i].style.backgroundColor){
-                cells[i].style.backgroundColor = updatedcolors[i];
-            }
-        }
+        UpdateCanvaColors(canvaStates[ptr]);
     }
 });
 
-let isDrawing = false;
-let showOutline = false;
-let pencilColor =  "red";
-document.querySelector(".redcrayon").classList.add("selected");
+function dfs(row, col, visited, allowedColor){
+    //validate coordinates 
+    if(row<0 || row >= GRID_SIZE || col<0 || col>=GRID_SIZE){
+        return;
+    }
+
+    // k : index of current cell in cells nodelist
+    k = GRID_SIZE*row + col; 
+
+    //check if cell already visited
+    if(visited.has(k)){
+        return;
+    }
+
+    //validate current cell color
+    let currentColor = cells[k].style.backgroundColor;
+    if(currentColor == allowedColor || currentColor ==  EMPTY_COLOR){
+
+        cells[k].style.backgroundColor = pencilColor;
+        stateChanged = true;
+    
+        visited.add(k); //update visited to avoid infinite loops
+    
+        const dx = [0,0,1,-1]; //change in row
+        const dy = [1,-1,0,0]; //change in col
+    
+        for (let i = 0; i < dx.length; i++) {
+            let newrow = dx[i]+row;
+            let newcol = dy[i] + col;
+            dfs(newrow,newcol, visited, allowedColor);
+        } 
+    }
+   
+}
+fillButton.addEventListener("click", function(){
+    if(fillBucketMode){
+        fillBucketMode = false;
+        fillButton.style.backgroundColor = "white";
+    }else{
+        fillBucketMode = true;
+        fillButton.style.backgroundColor = "lightcoral";
+    }
+});
 
 toggleGridbutton.addEventListener("click", function(){
     if(!showOutline){
@@ -89,17 +150,23 @@ crayons.forEach(btn=>btn.addEventListener("click", function(e){
 
 cells.forEach(btn=>btn.addEventListener("mousedown", function(e){
     e.preventDefault(); //prevents not-allowed cursor from appearing when dragging
-    console.log(e.target.offse)
     isDrawing = true;
     let currentcell = e.target;
-    if(currentcell.style.backgroundColor != pencilColor){
-        stateChanged = true;
-        currentcell.style.backgroundColor = pencilColor;
+    if(fillBucketMode){
+        let coord = getCoordinates(currentcell); //[0]: row, [1]:column
+        let visited = new Set();
+        dfs(coord[0],coord[1], visited, currentcell.style.backgroundColor);
+    }else{
+        if(currentcell.style.backgroundColor != pencilColor){
+            stateChanged = true;
+            currentcell.style.backgroundColor = pencilColor;
+        }
     }
 }));
 
 cells.forEach(btn=>btn.addEventListener("mousemove", function(e){
     e.preventDefault(); //prevents not-allowed cursor from appearing when dragging
+    if(fillBucketMode)return;
     if(isDrawing){
         let currentcell = e.target;
         if(currentcell.style.backgroundColor != pencilColor){
@@ -113,18 +180,23 @@ window.addEventListener("mouseup", function(){
     isDrawing = false;
     if(stateChanged){
         stateChanged = false;
+        //create a copy of current displayed frame's colors
         let colors = [];
         for (let i = 0; i < cells.length; i++) {
             colors.push(cells[i].style.backgroundColor);
         }        
-        console.log(colors);
+        // console.log(colors);
+
+        // if current frame is not the latest frame, remove all frames 
+        // after current one in canvaStates 
+        // undo/redo functionality similar to Paint.
         if(ptr != canvaStates.length-1){
             while(canvaStates.length-1 != ptr){
                 canvaStates.pop();
             }
         }
-        canvaStates.push(colors);
+        canvaStates.push(colors); //push copy to canvaStates.
         ptr = canvaStates.length-1;
-        console.log(canvaStates);
+        // console.log(canvaStates);
     }
 });
