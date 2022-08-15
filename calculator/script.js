5c859a29cfadc854d444c13134f620f60c911ce6
creme332 <65414576+creme332@users.noreply.github.com>
2022-08-15 09:53:14 +0400

    Refactor js file of calculator project

--- calculator/script.js
+++ calculator/script.js
@@ -2,33 +2,33 @@ const abacus = document.querySelector(".abacus");
 const numberOfColumns = 10;
 const beadsPerColumn = 10;
 const beadSize = 40; //px
-const columnHeight = beadSize*(beadsPerColumn+1); //px
+const columnHeight = beadSize * (beadsPerColumn + 1); //px
 const columnWidth = beadSize + 10; //px
-const columnColors = ["green", "red","hotpink","blue","orange"];
+const columnColors = ["green", "red", "hotpink", "blue", "orange"];
 let gapPosition = []; //gap position in each column. 
 
-const ballCollision = document.getElementById("ball-collision");
-ballCollision.volume = 0.2;
+const ballCollisionSound = document.getElementById("ball-collision");
+ballCollisionSound.volume = 0.2;
 
-console.assert(numberOfColumns>0 && beadsPerColumn>0, "Invalid abacus size");
-console.assert(columnHeight%beadSize==0, "Column height must be a multiple of bead size");
-console.assert(columnHeight/beadSize == (beadsPerColumn+1), "Extra space in each column should be equal to 1 bead size");
+console.assert(numberOfColumns > 0 && beadsPerColumn > 0, "Invalid abacus size");
+console.assert(columnHeight % beadSize == 0, "Column height must be a multiple of bead size");
+console.assert(columnHeight / beadSize == (beadsPerColumn + 1), "Extra space in each column should be equal to 1 bead size");
 
-//create counter container
+//create container to store each counter
 const counterContainer = document.createElement("div");
-counterContainer.className="counter-container"; 
+counterContainer.className = "counter-container";
 
-function buildAbacus(){
+function buildAbacus() {
     const columnContainer = document.createElement("div");
-    columnContainer.className="column-container";
+    columnContainer.className = "column-container";
 
-    for(let i=0;i<numberOfColumns;i++){
+    for (let i = 0; i < numberOfColumns; i++) {
         //initially, gap is found at top for each column
-        gapPosition.push(0); 
+        gapPosition.push(0);
 
         //create a counter for current column
         let counter = document.createElement("div");
-        counter.className="counter";
+        counter.className = "counter";
         counter.style.width = columnWidth + "px";
         counter.style.height = beadSize + "px";
         counter.textContent = gapPosition[i];
@@ -38,58 +38,60 @@ function buildAbacus(){
 
         //create column
         let column = document.createElement("div");
-        column.className="column";
-        column.style.height = columnHeight +"px";
-        column.style.width = columnWidth +"px";
+        column.className = "column";
+        column.style.height = columnHeight + "px";
+        column.style.width = columnWidth + "px";
 
         //create stick
         let stick = document.createElement("div");
-        stick.className="stick";
-        stick.style.height = columnHeight +"px";
+        stick.className = "stick";
+        stick.style.height = columnHeight + "px";
 
         // put stick in column
         column.appendChild(stick);
 
         //put beads in column
-        for(let j=0;j<beadsPerColumn;j++){
+        for (let j = 0; j < beadsPerColumn; j++) {
             let bead = document.createElement("div");
-            bead.className="bead";
+            bead.className = "bead";
             bead.style.height = beadSize + "px";
             bead.style.width = beadSize + "px";
-            bead.style.backgroundColor = columnColors[i%columnColors.length];
+            bead.style.backgroundColor = columnColors[i % columnColors.length];
             column.appendChild(bead);
         }
         //add column to columnContainer
         columnContainer.appendChild(column);
     }
-    abacus.appendChild(columnContainer);   
+    abacus.appendChild(columnContainer);
     abacus.appendChild(counterContainer);
 }
 buildAbacus();
-const beads =  document.querySelectorAll(".bead");
+const beads = document.querySelectorAll(".bead");
 
-function showBeadPos(){
-    beads.forEach(b=>{b.textContent=getBeadIndex(b);})
+function showBeadPos() {
+    beads.forEach(b => {
+        b.textContent = getBeadIndex(b);
+    })
 }
 
-function getBeadIndex(bead){
+function getBeadIndex(bead) {
     let currentColumn = bead.parentNode;
     let relativeY = bead.getBoundingClientRect().top - currentColumn.getBoundingClientRect().top;
-    let beadIndex = parseInt(relativeY/beadSize);
+    let beadIndex = parseInt(relativeY / beadSize);
     return beadIndex;
 }
 // showBeadPos();
-function getColumnIndex(column){
+function getColumnIndex(column) {
     let relativeX = column.getBoundingClientRect().left - abacus.getBoundingClientRect().left;
-    let columnIndex = parseInt(relativeX/columnWidth);
+    let columnIndex = parseInt(relativeX / columnWidth);
     return columnIndex;
 }
 
 //displace beads onclick
-beads.forEach(bead=>{
-    bead.addEventListener("click", e=>{
-        ballCollision.currentTime=0;
-        ballCollision.play();
+beads.forEach(bead => {
+    bead.addEventListener("click", e => {
+        ballCollisionSound.currentTime = 0;
+        ballCollisionSound.play();
         let clickedBead = e.target;
         let clickedColumn = clickedBead.parentNode; //column containing clicked bead
         let clickedBeadIndex = getBeadIndex(clickedBead); //position of clicked bead in clickedColumn. top-most position is index 0.
@@ -98,27 +100,27 @@ beads.forEach(bead=>{
         let currentColumnBeads = clickedColumn.querySelectorAll(".bead"); //all beads in current column
 
         //loop through each bead in current column and move it if needed
-        currentColumnBeads.forEach(cbead=>{
+        currentColumnBeads.forEach(cbead => {
             let cbeadPos = getBeadIndex(cbead);
-            if(clickedBeadIndex>ColumnGapIndex){//must displace beads upwards
+            if (clickedBeadIndex > ColumnGapIndex) { //must displace beads upwards
 
-                if(cbeadPos>ColumnGapIndex && cbeadPos<=clickedBeadIndex){ //current bead is below gap and above clicked bead
+                if (cbeadPos > ColumnGapIndex && cbeadPos <= clickedBeadIndex) { //current bead is below gap and above clicked bead
                     let currentY = 0;
-                    if(cbead.style.top != ""){
+                    if (cbead.style.top != "") {
                         currentY = parseInt(cbead.style.top);
                     }
                     currentY -= beadSize; // move beadSize units upwards
-                    cbead.style.top = currentY.toString() +"px";
+                    cbead.style.top = currentY.toString() + "px";
                 }
 
-            }else{//must displace beads downwards
-                if(cbeadPos<ColumnGapIndex && cbeadPos>=clickedBeadIndex){ //current bead is above gap and below clicked bead
+            } else { //must displace beads downwards
+                if (cbeadPos < ColumnGapIndex && cbeadPos >= clickedBeadIndex) { //current bead is above gap and below clicked bead
                     let currentY = 0;
-                    if(cbead.style.top != ""){
+                    if (cbead.style.top != "") {
                         currentY = parseInt(cbead.style.top);
                     }
                     currentY += beadSize; // move beadSize units downwards
-                    cbead.style.top = currentY.toString() +"px";
+                    cbead.style.top = currentY.toString() + "px";
                 }
             }
         });
@@ -127,10 +129,10 @@ beads.forEach(bead=>{
         // showBeadPos();
         //update counter 
         let currentCounter = counterContainer.querySelector(`div:nth-child(${currentColumnIndex+1})`);
-        currentCounter.textContent  = clickedBeadIndex;
-        if(clickedBeadIndex==10){
+        currentCounter.textContent = clickedBeadIndex;
+        if (clickedBeadIndex == 10) {
             currentCounter.classList.add("error-animation");
-        }else{
+        } else {
             currentCounter.classList.remove("error-animation");
         }
     });
