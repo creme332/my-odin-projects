bc1c7202918a5b8e39d413f9f5ce9afc5aa17dec
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-20 18:34:27 +0400

    sign-up-form : v2 of website
    add 3d fox, add password validation, restructure form

--- sign-up-form/script.js
+++ sign-up-form/script.js
@@ -1,7 +1,28 @@
-function checkPassword(){
-    const firstpass = document.getElementById("password").value;
-    const secondpass = document.getElementById("confirmpwd").value;
-    console.log(firstpass,secondpass)
-}
+const firstPasswordBox = document.getElementById("password");
+const secondPasswordBox = document.getElementById("confirmpwd");
 
-document.addEventListener("input",checkPassword)
+function checkPassword(){
+    if (firstPasswordBox.value != secondPasswordBox.value){
+        console.log("Passwords do not match")
+        secondPasswordBox.classList.add("wrong");
+        secondPasswordBox.parentElement.querySelector(".error-message").style.visibility="visible";
+        }else{
+            secondPasswordBox.classList.remove("wrong");
+            secondPasswordBox.parentElement.querySelector(".error-message").style.visibility="hidden";
+        console.log("YAY!")
+    }
+    console.log(firstPasswordBox.value,secondPasswordBox.value)
+}
+const msg = document.querySelector(".error-message");
+
+// Init a timeout variable to be used below
+let timeout = null;
+
+// Listen for keystroke events
+secondPasswordBox.addEventListener('keyup', function (e) {
+    https://schier.co/blog/wait-for-user-to-stop-typing-using-javascript
+    clearTimeout(timeout);
+
+    // Make a new timeout set to go off in 1000ms (1 second)
+    timeout = setTimeout(checkPassword, 1000);
+});
