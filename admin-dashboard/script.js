0542941f6d5f12599e63fb12b51cda2b4490e9f8
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-24 21:31:26 +0400

    admin : Complete a crude version of row 2

--- admin-dashboard/script.js
+++ admin-dashboard/script.js
@@ -14,7 +14,7 @@ toggleSidebarBtn.addEventListener("click", ()=>{
     }
 })
 
-const labels = [
+const line_chart_labels = [
     'January',
     'February',
     'March',
@@ -23,8 +23,8 @@ const labels = [
     'June',
   ];
 
-  const data = {
-    labels: labels,
+  const line_chart_data = {
+    labels: line_chart_labels,
     datasets: [{
       label: 'Visitors',
       backgroundColor: 'rgb(255, 99, 132)',
@@ -33,13 +33,114 @@ const labels = [
     }]
   };
 
-  const config = {
+  const line_chart_config = {
     type: 'line',
-    data: data,
+    data: line_chart_data,
     options: {responsive:true}
   };
 
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
+  const lineChart = new Chart(
+    document.getElementById('lineChart'),
+    line_chart_config
   );
+
+  const donut_chart_data = {
+    labels: [
+      'Python',
+      'JavaScript',
+      'C++'
+    ],
+    datasets: [{
+      label: 'Language',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const donut_chart_config = {
+    type: 'doughnut',
+    data: donut_chart_data,
+    options: {responsive:true}
+  };
+  const donutChart = new Chart(
+    document.getElementById('donutChart'),
+    donut_chart_config
+  );
+
+  const polar_data = {
+    labels: [
+      'Red',
+      'Green',
+      'Yellow',
+      'Grey',
+      'Blue'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [11, 16, 7, 3, 14],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ]
+    }]
+  };
+  const polar_config = {
+    type: 'polarArea',
+    data: polar_data,
+    options: {}
+  };
+  const polarChart = new Chart(
+    document.getElementById('polarChart'),
+    polar_config
+  );
+
+const labels = ['b','a','s','r','r','y','y'];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+const barChart = new Chart(
+  document.getElementById('barChart'),
+  config
+);
