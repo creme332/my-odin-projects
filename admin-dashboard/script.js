9931e59dea5b86f174e3caa7b062e329074ea801
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-24 20:06:30 +0400

    admin : complete first row of <main>

--- admin-dashboard/script.js
+++ admin-dashboard/script.js
@@ -13,3 +13,33 @@ toggleSidebarBtn.addEventListener("click", ()=>{
         sidebar.style.display = "flex";
     }
 })
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Visitors',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {responsive:true}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
