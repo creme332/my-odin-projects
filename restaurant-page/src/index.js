dda1e51dd2c6b36f81665b36e7924e664ba5f253
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-11 18:42:38 +0400

    resto : Create a bare-bones homepage without JS

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,2 +1,24 @@
-const el  = document.getElementById('content');
-el.textContent = 'All good';
+import './reset.css';
+import './style.css';
+
+import myLogo from './assets/giphy.gif';
+import font from './assets/marqueem.ttf';
+
+const new_font = new FontFace('marqueem', `url(${font})`);
+
+const el = document.getElementById('content');
+// el.textContent = 'menu';
+
+// Add the image to our existing div.
+const myIcon = new Image();
+myIcon.src = myLogo;
+
+// el.appendChild(myIcon);
+
+new_font.load().then(function (loaded_face) {
+  // use font here
+  document.fonts.add(loaded_face);
+  // el.style.fontFamily = 'marqueem';
+}).catch(function (error) {
+  alert('Font was not properly loaded !', error);
+});
--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
