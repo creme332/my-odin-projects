5085f7f32f0e4c6bba8601dc441f108f215e7986
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-13 15:21:01 +0400

    resto : add docstring to helper.js

--- restaurant-page/src/helper.js
+++ restaurant-page/src/helper.js
@@ -1,3 +1,12 @@
+/**
+ * Creates and returns an HTML element
+ * @param {string} type HTML type : div, nav, ul, ...
+ * @param {string} id HTML ID
+ * @param {[string]} arrayClasses 
+ * @param {string} txtContent 
+ * @param {[HTMLElement]} arrayChildren HTML elements which must be appended to newly created element
+ * @returns {HTMLElement} HTML element
+ */
 export function createHtmlElement(type, id, arrayClasses, txtContent, arrayChildren) {
     const element = document.createElement(type);
 
