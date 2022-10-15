33b7bc5adf98bfd0863b121b4d3b7ad8fb4fe6be
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-15 14:16:19 +0400

    resto : complete gallery tab

--- restaurant-page/src/gallery.js
+++ restaurant-page/src/gallery.js
@@ -1,6 +1,14 @@
-//import images
+//import images from gallery folder
+import dancers_img from './assets/gallery/dancers.png';
+import dj_img from './assets/gallery/dj.png';
+import food1_img from './assets/gallery/food1.png';
+import food2_img from './assets/gallery/food2.png';
+import peopleEating_img from './assets/gallery/people-eating.png';
+import randomGuy_img from './assets/gallery/random-guy.png';
+import resto_img from './assets/gallery/resto.png';
+import womanWhite_img from './assets/gallery/woman-in-white.png';
 
-import { createHtmlElement } from './helper';
+import { createHtmlElement, createImgElement } from './helper';
 
 const GalleryPageFactory = (main) => {
 
@@ -8,23 +16,49 @@ const GalleryPageFactory = (main) => {
         main.classList.add('gallery');
 
         //add images
-        addImages();
+        addRandomImages(20);
     };
 
     function removeGalleryTab() {
         main.classList.remove('gallery');
-        const gallery  = main.querySelector('.gallery-container');
+        const gallery = main.querySelector('.gallery-container');
         gallery.remove();
     };
 
-    function addImages(cardsArray) {
+    function getImgWithContainer(src, alt) {
+        const cardImg = createImgElement(src, alt, null, null);
+        const imgContainer = createHtmlElement('div', null, ['img-container'], null, [cardImg]);
+        return imgContainer;
+    }
+    function addRandomImages(imageCount) {
         const gallery = createHtmlElement('div', null, ['gallery-container'], null, null);
-        const imageCount = 5;
-        for(let i=0;i<imageCount;i++){
-            const cardImg = new Image();
-            cardImg.src = `https://source.unsplash.com/random/500x500?sig=${i}`;
-            cardImg.alt = 'a random image';
-            const imgContainer = createHtmlElement('div',null,['img-container'],null,[cardImg]);
+
+        let imgContainer = getImgWithContainer(resto_img, 'A chinese restaurant with a neon sign outside');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(dancers_img, 'A group of dancers in poorly lit night club');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(womanWhite_img, 'A woman in white posing seductively in a night club');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(randomGuy_img, 'Portrait of a black guy in a night club');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(food1_img, 'A plate with eggs');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(dj_img, 'A DJ in a nightclub');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(food2_img, 'A plate filled with veggies');
+        gallery.appendChild(imgContainer);
+
+        imgContainer = getImgWithContainer(peopleEating_img, 'People sitting in a calm environment');
+        gallery.appendChild(imgContainer);
+
+        for (let i = 0; i < imageCount; i++) {
+            imgContainer = getImgWithContainer(`https://source.unsplash.com/random/?nightclub&${Math.random()}`, 'A random image from Unsplash with keyword nightclub');
             gallery.appendChild(imgContainer);
         }
 
