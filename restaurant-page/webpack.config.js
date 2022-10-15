000307365d4917e89c10649db0fd75a891205bb9
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-15 15:08:33 +0400

    resto : save dist assets to a specific folder

--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
     clean: true,
   },
   module: {
@@ -24,10 +25,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }
       },
     ],
   },
