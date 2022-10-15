005887c5bdafa7da0337a85687ee0ea10ab4389b
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-15 17:12:15 +0400

    resto : switch to production mode

--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -2,18 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
     }),
   ],
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]',
     clean: true,
   },
   module: {
