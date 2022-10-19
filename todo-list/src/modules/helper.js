6ca309fb6d1c110a98e81c3be32d5699ad7a96fb
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-19 21:10:04 +0400

    todo : add a helper module to create html

---
+++ todo-list/src/modules/helper.js
@@ -0,0 +1,50 @@
+/**
+ * Creates and returns an HTML element.
+ * @param {string} type HTML type : div, nav, ul, ...
+ * @param {string} id HTML ID
+ * @param {[string]} arrayClasses 
+ * @param {string} txtContent 
+ * @param {[HTMLElement]} arrayChildren HTML elements which must be appended to newly created element
+ * @returns {HTMLElement} HTML element
+ */
+export function createHtmlElement(type, id, arrayClasses, txtContent, arrayChildren) {
+  const element = document.createElement(type);
+
+  if (id) element.id = id;
+
+  if (arrayClasses)
+    arrayClasses.forEach((myClass) => element.classList.add(myClass));
+
+  if (txtContent) element.textContent = txtContent;
+
+  if (arrayChildren) {
+    arrayChildren.forEach((child) => element.appendChild(child));
+  }
+
+  return element;
+}
+
+/**
+ * Returns an HTML card storing todo item main details.
+ * @param {Task} cardObj 
+ * @returns {HTMLElement}
+ */
+export function createCardElement(cardObj) {
+  const title = createHtmlElement('div', null, ['card-title'], cardObj.title, null);
+
+  let priorityClass = ['card-priority', 'highlight'];
+  let statusIndex = cardObj.getStatusIndex();
+  if (statusIndex == 0) {
+    priorityClass.push('high-priority');
+  }
+  if (statusIndex == 1) {
+    priorityClass.push('medium-priority');
+  }
+  if (statusIndex == 2) {
+    priorityClass.push('low-priority');
+  }
+  const priority = createHtmlElement('div', null, priorityClass, cardObj.priority, null);
+
+  const date = createHtmlElement('div', null, ['card-date'], cardObj.duedate, null);
+  return createHtmlElement('div', null, ['card'], null, [title, priority, date]);
+}
