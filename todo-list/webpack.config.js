3aa913c3455e70502b768f55f78a564f06665b55
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-26 14:58:04 +0400

    todo : deploy website in production mode

--- todo-list/webpack.config.js
+++ todo-list/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
