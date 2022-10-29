78bec41b78a8dbb9694302fcd7b3396584c82c3c
creme332 <65414576+creme332@users.noreply.github.com>
2022-10-29 15:45:29 +0400

    todo : install babel

--- todo-list/webpack.config.js
+++ todo-list/webpack.config.js
@@ -24,6 +24,18 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
