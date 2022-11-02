6d5d0156de6d789d83b9573f903aaf54e1c27a73
creme332 <65414576+creme332@users.noreply.github.com>
2022-11-02 17:03:29 +0400

    weather : create websitelayout and fill sidebar

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,3 +1,34 @@
 // import my styles
 import "./reset.css";
 import "./styles.css";
+
+async function getWeatherData(countryName) {
+  const API_KEY = "0ca9d53d2b652c10a5a8656ff1807c73";
+  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${countryName}&APPID=${API_KEY}`;
+
+  try {
+    const response = await fetch(URL);
+    // console.log(response);
+    const weather = await response.json();
+    console.log(weather);
+  } catch (err) {
+    console.log("teerre");
+    console.log(err);
+  }
+}
+async function getPollutionData(latitude, longitude) {
+  const API_KEY = "0ca9d53d2b652c10a5a8656ff1807c73";
+  const URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+  try {
+    const response = await fetch(URL);
+    const weather = await response.json();
+    console.log(weather);
+  } catch (err) {
+    console.log("teerre");
+    console.log(err);
+  }
+}
+
+// getWeatherData("Port-Louis");
+// getPollutionData(57,-20);
