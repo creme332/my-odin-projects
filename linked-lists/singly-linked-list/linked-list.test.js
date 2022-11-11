f9c86a808df122d567153cbc50ba3bef477ee4cf
creme332 <65414576+creme332@users.noreply.github.com>
2022-11-11 11:40:46 +0400

    LL : add more tests for singly LL

--- linked-lists/singly-linked-list/linked-list.test.js
+++ linked-lists/singly-linked-list/linked-list.test.js
@@ -4,7 +4,10 @@ import LinkedList from "./linked-list-class.js";
 import Node from "./node-class.js";
 
 describe("Test methods for empty linked list", () => {
-  const list = new LinkedList();
+  let list;
+  beforeEach(() => {
+    list = new LinkedList();
+  });
 
   test("toString()", () => {
     expect(list.toString()).toBe("∅");
@@ -28,67 +31,111 @@ describe("Test methods for empty linked list", () => {
   });
 
   test("prepend()", () => {
-    const newList = new LinkedList();
     const n1 = new Node("🍕");
-    newList.prepend(n1);
-    expect(newList.toString()).toBe("( 🍕 ) ➔ ∅");
+    list.prepend(n1);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ∅");
   });
 
   test("append()", () => {
-    const newList = new LinkedList();
     const n1 = new Node("🍕");
-    newList.append(n1);
-    expect(newList.toString()).toBe("( 🍕 ) ➔ ∅");
+    list.append(n1);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ∅");
   });
 
   test("pop()", () => {
     list.pop();
     expect(list.toString()).toBe("∅");
   });
+
+  test("find()", () => {
+    expect(list.find("🍕")).toBe(null);
+  });
+});
+
+describe("Test methods for non-empty linked list", () => {
+  let list;
+  beforeEach(() => {
+    list = new LinkedList(["🍕", "🥟"]);
+  });
+
+  test("append 1 element to non-empty list", () => {
+    const n1 = new Node("🍘");
+    list.append(n1);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ( 🍘 ) ➔ ∅");
+  });
+
+  test("prepend 1 element to non-empty list", () => {
+    const n1 = new Node("🍘");
+    list.prepend(n1);
+    expect(list.toString()).toBe("( 🍘 ) ➔ ( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
+  });
+
+  test("Get node at a valid index", () => {
+    const json =  JSON.stringify(list.at(1));
+    console.log(json);
+    expect(json).toBe(`{"_data":"🥟","_next":null}`);
+  });
+
+  test("Get node at invalid index", () => {
+    expect(list.at(3)).toBe(null);
+  });
+
+  test("pop()", () => {
+    list.pop();
+    expect(list.toString()).toBe("( 🍕 ) ➔ ∅");
+  });
+
+  test("contains() for a valid value", () => {
+    expect(list.contains('🍕')).toBe(true);
+  });
+
+  test("contains() for an invalid value", () => {
+    expect(list.contains('hello')).toBe(false);
+  });
+
+  test("insertAt() for an invalid value", () => {
+    list.insertAt("🐓", 100);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
+  });
+
+  test("Insert at start", () => {
+    list.insertAt("🐓", 0);
+    expect(list.toString()).toBe("( 🐓 ) ➔ ( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
+  });
+
+  test("Insert between 2 elements", () => {
+    list.insertAt("🐓", 1);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🐓 ) ➔ ( 🥟 ) ➔ ∅");
+  });
+
+  test("Insert after end of list", () => {
+    list.insertAt("🐓", 2);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ( 🐓 ) ➔ ∅");
+  });
+
+  test("Insert at invalid index", () => {
+    list.insertAt("🐓", 100);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
+  });
+
+  test("Remove at start", () => {
+    list.removeAt(0);
+    expect(list.toString()).toBe("( 🥟 ) ➔ ∅");
+  });
+
+  test("Remove at end", () => {
+    list.removeAt(1);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ∅");
+  });
+
+  test("Remove at invalid index", () => {
+    list.removeAt(100);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
+  });
+
 });
 
 test("Initialise list with array", () => {
-  const list = new LinkedList(["🍕", "🥟"]);
-  expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
-});
-
-test("append 1 element to non-empty list", () => {
-  const list = new LinkedList();
-  const n1 = new Node("🍕");
-  const n2 = new Node("🥟");
-  list.append(n1);
-  list.append(n2);
-  expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ∅");
-});
-
-test("prepend 1 element to non-empty list", () => {
-  const list = new LinkedList();
-  const n1 = new Node("🍕");
-  const n2 = new Node("🥟");
-  list.prepend(n1);
-  list.prepend(n2);
-  expect(list.toString()).toBe("( 🥟 ) ➔ ( 🍕 ) ➔ ∅");
-});
-
-test("prepend 1 element to non-empty list", () => {
-  const list = new LinkedList();
-  const n1 = new Node("🍕");
-  const n2 = new Node("🥟");
-  list.prepend(n1);
-  list.prepend(n2);
-  expect(list.toString()).toBe("( 🥟 ) ➔ ( 🍕 ) ➔ ∅");
-});
-
-test("Get node at valid index", () => {
-  const list = new LinkedList();
-  const n1 = new Node("🍕");
-  const n2 = new Node("🥟");
-  list.append(n1);
-  list.append(n2);
-  expect(list.at(1)).toBe(n2);
-});
-
-test("Get node at invalid index", () => {
-  const list = new LinkedList(["🍕", "🥟"]);
-  expect(list.at(3)).toBe(null);
+    const list = new LinkedList (['🍕','🥟','🍘']);
+    expect(list.toString()).toBe("( 🍕 ) ➔ ( 🥟 ) ➔ ( 🍘 ) ➔ ∅");
 });
