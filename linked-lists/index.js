9609fe55de8b20b667990c83b4063946514ab2fd
creme332 <65414576+creme332@users.noreply.github.com>
2022-11-11 13:11:39 +0400

    LL : add output to `index.js`

--- linked-lists/index.js
+++ linked-lists/index.js
@@ -1,19 +1,20 @@
 import LinkedList from "./singly-linked-list/linked-list-class.js";
 import Node from "./singly-linked-list/node-class.js";
 
-const n1 = new Node("🍕");
-const n2 = new Node("🍿");
-const n3 = new Node("🥚");
-const n4 = new Node("🍆");
-const list = new LinkedList();
+const list = new LinkedList(["🍕", "🍿", "🥚", "🍆"]);
 list.output("Initial linked list:");
-list.append(n1);
-console.log(list);
-list.prepend(n4);
-list.append(n2);
-list.prepend(n3);
-console.log("Node at index 2:", list.at(2));
-console.log("Index of node with value 🐓 :", list.find("🐓"));
-list.insertAt("🐓", 1);
-console.log("Index of node with value 🐓 :", list.find("🐓"));
-list.removeAt(5);
+
+list.prepend(new Node("🍷"));
+list.output("Prepended 🍷");
+
+list.reverse();
+list.output("Reversed linked list:");
+
+/*
+Initial linked list:
+( 🍕 ) ➔ ( 🍿 ) ➔ ( 🥚 ) ➔ ( 🍆 ) ➔ ∅
+Prepended 🍷
+( 🍷 ) ➔ ( 🍕 ) ➔ ( 🍿 ) ➔ ( 🥚 ) ➔ ( 🍆 ) ➔ ∅
+Reversed linked list:
+( 🍆 ) ➔ ( 🥚 ) ➔ ( 🍿 ) ➔ ( 🍕 ) ➔ ( 🍷 ) ➔ ∅
+*/
