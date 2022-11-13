0cc24920da17bba80055850135fb75dbc3547a49
creme332 <65414576+creme332@users.noreply.github.com>
2022-11-13 21:04:01 +0400

    knights : prettyprint shortest path to console

--- knights-travails/src/scripts/controller.js
+++ knights-travails/src/scripts/controller.js
@@ -16,9 +16,34 @@ const controller = (() => {
       view.getCarrotPosition()
     );
     const neighbours = model.neighbourStorage.get();
-    console.log("Minimum number of moves : ", shortestPath.length - 1);
-    console.log(JSON.stringify(shortestPath));
+    console.log(
+      prettyPrintPath(
+        view.getHorsePosition(),
+        view.getCarrotPosition(),
+        shortestPath
+      )
+    );
     view.playMoves(shortestPath, neighbours);
+  }
+
+  function prettyPrintPath(start, end, shortestPath) {
+    function indexToCoordinates(index) {
+      const row = parseInt(index / 8, 10);
+      const col = parseInt(index % 8, 10);
+      return [row, col];
+    }
+    const startCoordinate = JSON.stringify(indexToCoordinates(start));
+    const endCoordinate = JSON.stringify(indexToCoordinates(end));
+    let str = `${startCoordinate} -> ${endCoordinate} in ${
+      shortestPath.length - 1
+    } moves:\n`;
+
+    const coordinatesList = [];
+    shortestPath.forEach((i) => {
+      coordinatesList.push(indexToCoordinates(i));
+    });
+    str += JSON.stringify(coordinatesList);
+    return str;
   }
   return { animate };
 })();
