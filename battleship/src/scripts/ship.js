e94e644f00006f3c55c74e2591d838514bebac01
creme332 <65414576+creme332@users.noreply.github.com>
2022-12-11 21:40:05 +0400

    battleship : fix bug in Ship class and add few more methods

--- battleship/src/scripts/ship.js
+++ battleship/src/scripts/ship.js
@@ -24,8 +24,8 @@ class Ship {
    * `coord[1]` : Position of ship on particular board. (`0`-`99`)
    */
   constructor(size, verticallyOriented, coord) {
-    if (!(size >= 0 && size <= 4)) {
-      throw Error("Invalid ship size", size);
+    if (!(size > 0 && size < 5)) {
+      throw new Error("Invalid ship size", size);
     }
 
     if (
@@ -33,9 +33,9 @@ class Ship {
         (coord[0] === 0 || coord[0] === 1) && // board index either 0 or 1
         coord[1] >= 0 &&
         coord[1] < 100
-      ) //
+      )
     ) {
-      throw Error("Invalid ship coordinates", coord);
+      throw new Error("Invalid ship coordinates", coord);
     }
     this._size = size;
     this._vertical = verticallyOriented;
@@ -46,10 +46,26 @@ class Ship {
     const BOARD_SIZE = 10;
 
     for (let i = 0; i < this._size; i++) {
-      const cellPos = headPos + this._vertical ? BOARD_SIZE * i : i;
+      const cellPos = headPos + (this._vertical ? BOARD_SIZE * i : i);
       const cell = new ShipCell(cellPos);
       this._cellsArray.push(cell);
     }
+  }
+
+  get vertical() {
+    return this._vertical;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get coordinates() {
+    return this._coordinates;
+  }
+
+  get cellsArray(){
+    return this._cellsArray;
   }
 
   rotate() {
