9f1f870ebe5cb8b7a500c6b1f45f5d9797aaa966
creme332 <65414576+creme332@users.noreply.github.com>
2022-12-17 18:58:05 +0400

    battleship : add more tests for ship class

--- battleship/src/scripts/ship.test.js
+++ battleship/src/scripts/ship.test.js
@@ -2,15 +2,28 @@
 
 import Ship from "./ship";
 
-describe("Test ship class", () => {
-  test("vertical cruiser", () => {
+describe("Ship class", () => {
+  test("basic methods for vertical cruiser", () => {
     const cruiser = new Ship(4, true, [0, 1]);
+    const cellCoordinates = cruiser.cellsArray.map((el) => el.position);
+    console.log(cellCoordinates);
     expect(cruiser.size).toBe(4);
+    expect(cruiser.sunk()).toBe(false);
     expect(cruiser.vertical).toBe(true);
     expect(cruiser.coordinates).toStrictEqual([0, 1]);
-    // expect(cruiser.cellsArray).toStrictEqual([1, 11, 22, 33]);
-  });
+    expect(cellCoordinates).toStrictEqual([1, 11, 21, 31]);
 
+    cruiser.attack(31);
+    expect(cruiser.sunk()).toBe(false);
+    cruiser.attack(1);
+    cruiser.attack(11);
+    cruiser.attack(21);
+    expect(cruiser.sunk()).toBe(true);
+  });
+  test("ship rotation", () => {
+    const cruiser = new Ship(4, true, [0, 1]);
+    
+  });
   test("ship of invalid size", () => {
     let cruiser;
     const createShip = (size) => {
