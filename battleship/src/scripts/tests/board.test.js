72afca5f5fb2ba8bfecfde522103db3259199848
creme332 <65414576+creme332@users.noreply.github.com>
2022-12-25 16:22:31 +0400

    battleship: add tests for ship rotation, board reset

--- battleship/src/scripts/tests/board.test.js
+++ battleship/src/scripts/tests/board.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import Board from "../board";
-
+import Ship from "../ship";
 describe("board validation", () => {
   test("empty board", () => {
     const board = [
@@ -113,4 +113,58 @@ describe("board validation", () => {
     ];
     expect(Board.validate(board)).toBeFalsy();
   });
+});
+
+describe("ship rotation", () => {
+  const boardIndex = 0;
+  const shipArray = [
+    new Ship(4, true, boardIndex, 0),
+    new Ship(3, true, boardIndex, 3),
+    new Ship(3, false, boardIndex, 5),
+    new Ship(2, false, boardIndex, 38),
+    new Ship(2, true, boardIndex, 60),
+    new Ship(2, true, boardIndex, 84),
+    new Ship(1, false, boardIndex, 99),
+    new Ship(1, true, boardIndex, 19),
+    new Ship(1, true, boardIndex, 90),
+    new Ship(1, false, boardIndex, 59),
+  ];
+  const myBoard = new Board();
+  myBoard.loadShips(shipArray);
+
+  expect(myBoard.rotateShip(shipArray[0])).toBeFalsy();
+  expect(myBoard.rotateShip(shipArray[2])).toBeTruthy();
+  expect(myBoard.rotateShip(shipArray[5])).toBeTruthy();
+});
+
+describe("reset board", () => {
+  const boardIndex = 0;
+  const shipArray = [
+    new Ship(4, true, boardIndex, 0),
+    new Ship(3, true, boardIndex, 3),
+    new Ship(3, false, boardIndex, 5),
+    new Ship(2, false, boardIndex, 38),
+    new Ship(2, true, boardIndex, 60),
+    new Ship(2, true, boardIndex, 84),
+    new Ship(1, false, boardIndex, 99),
+    new Ship(1, true, boardIndex, 19),
+    new Ship(1, true, boardIndex, 90),
+    new Ship(1, false, boardIndex, 59),
+  ];
+  const myBoard = new Board();
+  myBoard.loadShips(shipArray);
+  myBoard.resetBoard();
+  expect(myBoard._shipArray).toStrictEqual([]);
+  expect(myBoard._board).toStrictEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
 });
