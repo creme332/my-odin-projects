c39d2e60d535fa77d0ae2fed44368232b015f04c
creme332 <65414576+creme332@users.noreply.github.com>
2022-12-30 20:52:59 +0400

    battleship: generate random fleet

--- battleship/src/scripts/model.js
+++ battleship/src/scripts/model.js
@@ -1,3 +1,6 @@
+// import Ship from "./ship.js";
+// import Board from "./board.js";
+
 import Ship from "./ship";
 import Board from "./board";
 
@@ -13,9 +16,10 @@ const model = (() => {
     // reset boards
     myBoard.resetBoard();
     rivalBoard.resetBoard();
+
     // load ships
     myBoard.loadShips(getDefaultFleet(0));
-    rivalBoard.loadShips(getDefaultFleet(1));
+    rivalBoard.loadShips(getRandomFleet(1));
   }
 
   function gameStarted() {
@@ -88,7 +92,7 @@ const model = (() => {
   }
 
   function computerPlay() {
-    if (getTurn()) throw new Error('Not computer turn to play');
+    if (getTurn()) throw new Error("Not computer turn to play");
     let randomMove = 0;
     while (!attackBoard(randomMove)) {
       randomMove++;
@@ -118,6 +122,109 @@ const model = (() => {
       rivalGuesses.push(pos);
     }
     return true;
+  }
+
+  /**
+   * Returns a valid fleet of ship.
+   * @param {int} boardIndex
+   * @returns {[Ship]}
+   */
+  function getRandomFleet(boardIndex) {
+    /**
+     * Returns a valid ship that fits board and does not touch other ships.
+     * @param {int} shipSize Integer 1 - 4 inclusive
+     * @param {int} boardIndex Integer 0 - 1 inclusive
+     * @returns {Ship}
+     */
+    function getRandomShip(shipSize, boardNum, freePositions, shipsPositions) {
+      let validShip = false;
+      let ship;
+      const size = freePositions.length;
+      while (!validShip) {
+        const randomOrientation = Math.random() < 0.5;
+        const randomPos = freePositions[Math.floor(Math.random() * size)];
+        try {
+          ship = new Ship(shipSize, randomOrientation, boardNum, randomPos);
+
+          // is there any ship cell which is not free
+          const shipCellsAvailable = !ship
+            .getCellPositions()
+            .some((pos) => !freePositions.includes(pos));
+
+          // A ship is isolated iff all surrounding cells are not ship cells
+          const shipIsIsolated = !ship
+            .getCellPositions()
+            .some((pos) =>
+              getTouchingCells(pos).some((x) => shipsPositions.includes(x))
+            );
+
+          validShip = shipCellsAvailable && shipIsIsolated;
+        } catch (err) {
+          validShip = false;
+        }
+      }
+      return ship;
+    }
+
+    /**
+     * Returns the positions of all cells surrounding current cell.
+     * @param {int} pos cell position 0 - 99
+     * @returns {[int]} array of positions
+     */
+    function getTouchingCells(pos) {
+      if (pos < 0 || pos >= Board.BOARD_SIZE * Board.BOARD_SIZE) {
+        throw new Error(`Invalid cell position: ${pos}`);
+      }
+      const cells = [];
+      const row = parseInt(pos / Board.BOARD_SIZE, 10);
+      const col = pos % Board.BOARD_SIZE;
+      const translation = [
+        [-1, -1, -1, 0, 1, 1, 1, 0],
+        [-1, 0, 1, 1, 1, 0, -1, -1],
+      ];
+      for (let i = 0; i < translation[0].length; i++) {
+        const r = row + translation[0][i];
+        const c = col + translation[1][i];
+        if (r >= 0 && c >= 0 && r < Board.BOARD_SIZE && c < Board.BOARD_SIZE) {
+          cells.push(r * Board.BOARD_SIZE + c);
+        }
+      }
+      return cells;
+    }
+    const shipSizes = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+    let freePositions = Array.from(
+      Array(Board.BOARD_SIZE * Board.BOARD_SIZE).keys()
+    ); // All positions where ship can be placed [0, 1, 2, 3, ... , 99]
+    let shipPositions = [];
+    const fleet = [];
+
+    shipSizes.forEach((shipSize) => {
+      // displayBoard(shipPositions);
+      // console.log(`Solving ${shipSize}`);
+      // get a random valid ship
+      const shipObj = getRandomShip(
+        shipSize,
+        boardIndex,
+        freePositions,
+        shipPositions
+      );
+      // console.log(`Solved ${shipSize}`);
+
+      fleet.push(shipObj);
+
+      shipPositions = [...shipPositions, ...shipObj.getCellPositions()];
+
+      // update isFree (remove ship cell positions and surrounding cells)
+      shipObj.getCellPositions().forEach((pos) => {
+        const surroundingCells = getTouchingCells(pos);
+        freePositions = freePositions.filter(
+          (el) => el !== pos && !surroundingCells.includes(el)
+        );
+        // console.log(freePositions);
+      });
+    });
+
+    return fleet;
   }
 
   function getDefaultFleet(boardIndex) {
@@ -185,8 +292,65 @@ const model = (() => {
     gameStarted,
     initialiseBoards,
     isShip,
-    computerPlay
+    computerPlay,
+    getRandomFleet,
   };
 })();
 
+// function displayBoard(shipPos) {
+//   const board = [...Array(10)].map(() => Array(10).fill(0));
+//   for (let i = 0; i < 100; i++) {
+//     if (shipPos.includes(i)) {
+//       board[parseInt(i / 10, 10)][i % 10] = 1;
+//     }
+//   }
+//   console.log(board);
+// }
 export default model;
+// const y = model.getRandomFleet(0);
+// const t = new Board();
+// t.loadShips(y);
+// console.log(y);
+
+// function getTouchingCells(pos) {
+//   if (pos < 0 || pos >= 100) {
+//     throw new Error(`Invalid cell position: ${pos}`);
+//   }
+//   const cells = [];
+//   const row = parseInt(pos / 10, 10);
+//   const col = pos % 10;
+//   const translation = [
+//     [-1, -1, -1, 0, 1, 1, 1, 0],
+//     [-1, 0, 1, 1, 1, 0, -1, -1],
+//   ];
+//   for (let i = 0; i < translation[0].length; i++) {
+//     const r = row + translation[0][i];
+//     const c = col + translation[1][i];
+//     if (r >= 0 && c >= 0 && r < 10 && c < 10) {
+//       cells.push(r * 10 + c);
+//     }
+//   }
+//   return cells;
+// }
+
+// const shipPositions = [245];
+
+// const p = [11, 12].some((pos) => {
+//   const surroundingCells = getTouchingCells(pos);
+//   console.log(`For ship cell ${pos}: `);
+
+//   // is there a surrounding cell which is a ship
+//   const ans = getTouchingCells(pos).some((x) => {
+//     // is surrounding cell free?
+//     const isShip = shipPositions.includes(x);
+//     if (isShip) {
+//       console.log(`${x} is a ship`);
+//     }
+//     return isShip;
+//   });
+//   if (ans) {
+//     console.log(`Surrounding cells of ${pos} contain ship`);
+//   }
+//   return ans;
+// });
+// console.log("ship invalid: ", p);
