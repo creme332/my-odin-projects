58ba7de290ce78f51b96bf7de7dd063932e24a03
creme332 <65414576+creme332@users.noreply.github.com>
2022-12-31 10:37:25 +0400

    battleship: add terserplugin and format `webpack.config.js`

--- battleship/webpack.config.js
+++ battleship/webpack.config.js
@@ -1,65 +1,81 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/scripts/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/template.html',
-        }),
+  mode: "production",
+  entry: "./src/scripts/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/template.html",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            pure_funcs: [
+              "console.log",
+              "console.info",
+              "console.debug",
+              "console.warn",
+            ],
+          },
+        },
+      }),
     ],
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8080,
-        hot: true
-    },
-    devtool: 'inline-source-map',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                  }
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[hash][ext][query]'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                  {
-                    loader: 'html-loader'
-                  }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[hash][ext][query]'
-                }
-            }
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
         ],
-    },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]",
+        },
+      },
+    ],
+  },
 };
