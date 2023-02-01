a95230f9f5f06d823874261445f3b8c551563177
creme332 <65414576+creme332@users.noreply.github.com>
2023-02-01 12:41:21 +0400

    update imports in `index.js`

--- cv-project/src/index.js
+++ cv-project/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
