714650a6e45403074f4349b9de3de2f06a7e2f62
creme332 <65414576+creme332@users.noreply.github.com>
2023-02-12 21:54:53 +0400

    complete code for dynamic employment history

--- cv-project/src/components/UserForm/Employment.js
+++ cv-project/src/components/UserForm/Employment.js
@@ -1,23 +1,108 @@
 import React, { Component } from "react";
 import { Form, InputGroup, Button, Col, Row, Accordion } from "react-bootstrap";
+import uniqid from "uniqid";
 
 class Employment extends Component {
   constructor() {
     super();
 
     this.state = {
-      prof_summary: "",
+      emp_boxes: [
+        {
+          box_id: uniqid(),
+          job_title: "",
+          employer: "",
+          start_date: "",
+          end_date: "",
+          city: "",
+        },
+      ],
     };
+
+    this.addEmploymentBox = this.addEmploymentBox.bind(this);
+    this.removeEmploymentBox = this.removeEmploymentBox.bind(this);
+    // this.updateJobTitle = this.updateJobTitle.bind(this);
   }
 
-  setSummary(e) {
+  updateJobTitle(e, box_id) {
     this.setState({
-      prof_summary: e.target.value.trim(),
+      emp_boxes: this.state.emp_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          console.log({ ...emp, job_title: e.target.value });
+          return { ...emp, job_title: e.target.value.trim() };
+        }
+        return emp;
+      }),
     });
-    console.log(this.state);
+  }
+
+  updateEmployer(e, box_id) {
+    this.setState({
+      emp_boxes: this.state.emp_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, employer: e.target.value.trim() };
+        }
+        return emp;
+      }),
+    });
+  }
+
+  updateStartDate(e, box_id) {
+    this.setState({
+      emp_boxes: this.state.emp_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, start_date: e.target.value };
+        }
+        return emp;
+      }),
+    });
+  }
+
+  updateEndDate(e, box_id) {
+    this.setState({
+      emp_boxes: this.state.emp_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, end_date: e.target.value };
+        }
+        return emp;
+      }),
+    });
+  }
+
+  updateCity(e, box_id) {
+    this.setState({
+      emp_boxes: this.state.emp_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, city: e.target.value.trim() };
+        }
+        return emp;
+      }),
+    });
+  }
+
+  addEmploymentBox() {
+    const default_box = {
+      box_id: uniqid(),
+      job_title: "",
+      employer: "",
+      start_date: "",
+      end_date: "",
+      city: "",
+    };
+    this.setState({
+      emp_boxes: this.state.emp_boxes.concat(default_box),
+    });
+  }
+
+  removeEmploymentBox(e, box_id) {
+    this.setState({
+      emp_boxes: this.state.emp_boxes.filter((box) => box.box_id !== box_id),
+    });
   }
 
   render() {
+    const { emp_boxes } = this.state;
+
     return (
       <Form.Group className="mb-3" controlId="EmploymentHistory">
         <h3> Employment History</h3>
@@ -28,38 +113,72 @@ class Employment extends Component {
         </Form.Text>
 
         <Accordion defaultActiveKey="0">
-          <Accordion.Item className="mb-3" eventKey="0">
-            <Accordion.Header>(Not specified)</Accordion.Header>
-            <Accordion.Body>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formBasicJobTitle">
-                  <Form.Label>Job title</Form.Label>
-                  <Form.Control className="blue-bg" type="text" />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formBasicEmployer">
-                  <Form.Label>Employer</Form.Label>
-                  <Form.Control className="blue-bg" type="text" />
-                </Form.Group>
-              </Row>
+          {emp_boxes.map((emp) => {
+            return (
+              <Accordion.Item key={emp.box_id} className="mb-3" eventKey="0">
+                <Accordion.Header>
+                  {emp.job_title === "" ? "(Not specified)" : emp.job_title}
+                </Accordion.Header>
 
-              <Row className="mb-3">
-                <InputGroup className="mb-3">
-                  <InputGroup.Text>Start & End date</InputGroup.Text>
-                  <Form.Control className="blue-bg" type="date" />
-                  <Form.Control className="blue-bg" type="date" />
-                </InputGroup>
+                <Accordion.Body>
+                  <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formBasicJobTitle">
+                      <Form.Label>Job title</Form.Label>
+                      <Form.Control
+                        onChange={(e) => this.updateJobTitle(e, emp.box_id)}
+                        className="blue-bg"
+                        type="text"
+                      />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formBasicEmployer">
+                      <Form.Label>Employer</Form.Label>
+                      <Form.Control
+                        onChange={(e) => this.updateEmployer(e, emp.box_id)}
+                        className="blue-bg"
+                        type="text"
+                      />
+                    </Form.Group>
+                  </Row>
 
-                <Form.Group as={Col} controlId="formBasicCity">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control className="blue-bg" type="text" />
-                </Form.Group>
-              </Row>
-              <Button variant="danger">Delete</Button>
-            </Accordion.Body>
-          </Accordion.Item>
+                  <Row className="mb-3">
+                    <InputGroup className="mb-3">
+                      <InputGroup.Text>Start & End date</InputGroup.Text>
+                      <Form.Control
+                        onChange={(e) => this.updateStartDate(e, emp.box_id)}
+                        className="blue-bg"
+                        type="date"
+                      />
+                      <Form.Control
+                        onChange={(e) => this.updateEndDate(e, emp.box_id)}
+                        className="blue-bg"
+                        type="date"
+                      />
+                    </InputGroup>
+
+                    <Form.Group as={Col} controlId="formBasicCity">
+                      <Form.Label>City</Form.Label>
+                      <Form.Control
+                        onChange={(e) => this.updateCity(e, emp.box_id)}
+                        className="blue-bg"
+                        type="text"
+                      />
+                    </Form.Group>
+                  </Row>
+                  <Button
+                    onClick={(e) => this.removeEmploymentBox(e, emp.box_id)}
+                    variant="danger"
+                  >
+                    Delete
+                  </Button>
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
         </Accordion>
 
-        <Button variant="secondary">+ Add one more employment</Button>
+        <Button onClick={this.addEmploymentBox} variant="secondary">
+          + Add one more employment
+        </Button>
       </Form.Group>
     );
   }
