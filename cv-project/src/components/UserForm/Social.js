769ccf01138b72967841ff597e732acce48564d7
creme332 <65414576+creme332@users.noreply.github.com>
2023-02-18 21:58:58 +0400

    cv : complete section on social links

--- cv-project/src/components/UserForm/Social.js
+++ cv-project/src/components/UserForm/Social.js
@@ -1,23 +1,69 @@
 import React, { Component } from "react";
 import { Form, Button, Col, Row, Accordion } from "react-bootstrap";
+import uniqid from "uniqid";
 
 class Social extends Component {
   constructor() {
     super();
 
     this.state = {
-      prof_summary: "",
+      social_boxes: [
+        {
+          box_id: uniqid(),
+          link_name: "",
+          url: "",
+        },
+      ],
     };
+
+    this.addBox = this.addBox.bind(this);
+    this.removeBox = this.removeBox.bind(this);
   }
 
-  setSummary(e) {
+  removeBox(e, box_id) {
     this.setState({
-      prof_summary: e.target.value.trim(),
+      social_boxes: this.state.social_boxes.filter(
+        (box) => box.box_id !== box_id
+      ),
     });
-    console.log(this.state);
+  }
+
+  addBox() {
+    const default_box = {
+      box_id: uniqid(),
+      link_name: "",
+      url: "",
+    };
+    this.setState({
+      social_boxes: this.state.social_boxes.concat(default_box),
+    });
+  }
+
+  updateLabel(e, box_id) {
+    this.setState({
+      social_boxes: this.state.social_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, link_name: e.target.value };
+        }
+        return emp;
+      }),
+    });
+  }
+
+  updateLink(e, box_id) {
+    this.setState({
+      social_boxes: this.state.social_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, url: e.target.value };
+        }
+        return emp;
+      }),
+    });
   }
 
   render() {
+    const { social_boxes } = this.state;
+
     return (
       <Form.Group className="mb-3" controlId="SocialLinks">
         <h3> Websites & Social Links</h3>
@@ -28,25 +74,52 @@ class Social extends Component {
         </Form.Text>
 
         <Accordion defaultActiveKey="0">
-          <Accordion.Item className="mb-3" eventKey="0">
-            <Accordion.Header>(Not specified)</Accordion.Header>
-            <Accordion.Body>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formBasicLabel">
-                  <Form.Label>Label</Form.Label>
-                  <Form.Control className="blue-bg" type="text" />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formBasicLink">
-                  <Form.Label>Link</Form.Label>
-                  <Form.Control className="blue-bg" type="text" />
-                </Form.Group>
-              </Row>
-              <Button variant="danger">Delete</Button>
-            </Accordion.Body>
-          </Accordion.Item>
+          {social_boxes.map((box) => {
+            return (
+              <Accordion.Item
+                className="mb-3"
+                key={box.box_id}
+                eventKey={box.box_id}
+              >
+                <Accordion.Header>
+                  {" "}
+                  {box.link_name === "" ? "(Not specified)" : box.link_name}
+                </Accordion.Header>
+                <Accordion.Body>
+                  <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formBasicLabel">
+                      <Form.Label>Label</Form.Label>
+                      <Form.Control
+                        onChange={(e) => this.updateLabel(e, box.box_id)}
+                        className="blue-bg"
+                        type="text"
+                      />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formBasicLink">
+                      <Form.Label>Link</Form.Label>
+                      <Form.Control
+                        onChange={(e) => this.updateLink(e, box.box_id)}
+                        className="blue-bg"
+                        type="text"
+                      />
+                    </Form.Group>
+                  </Row>
+
+                  <Button
+                    onClick={(e) => this.removeBox(e, box.box_id)}
+                    variant="danger"
+                  >
+                    Delete
+                  </Button>
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
         </Accordion>
 
-        <Button variant="secondary">+ Add one more link</Button>
+        <Button onClick={this.addBox} variant="secondary">
+          + Add one more link
+        </Button>
       </Form.Group>
     );
   }
