ccced08d0441e9a03e15959127bacaec3137e893
creme332 <65414576+creme332@users.noreply.github.com>
2023-02-18 21:47:46 +0400

    cv : complete skills section

--- cv-project/src/components/UserForm/Skills.js
+++ cv-project/src/components/UserForm/Skills.js
@@ -1,23 +1,56 @@
 import React, { Component } from "react";
 import { Form, Button, Row, Accordion } from "react-bootstrap";
+import uniqid from "uniqid";
 
 class Skills extends Component {
   constructor() {
     super();
 
     this.state = {
-      prof_summary: "",
+      skill_boxes: [
+        {
+          box_id: uniqid(),
+          skill_name: "",
+        },
+      ],
     };
+
+    this.addBox = this.addBox.bind(this);
+    this.removeBox = this.removeBox.bind(this);
   }
 
-  setSummary(e) {
+  updateBox(e, box_id) {
     this.setState({
-      prof_summary: e.target.value.trim(),
+      skill_boxes: this.state.skill_boxes.map((emp) => {
+        if (emp.box_id === box_id) {
+          return { ...emp, skill_name: e.target.value };
+        }
+        return emp;
+      }),
     });
-    console.log(this.state);
+  }
+
+  addBox() {
+    const default_box = {
+      box_id: uniqid(),
+      skill_name: "",
+    };
+    this.setState({
+      skill_boxes: this.state.skill_boxes.concat(default_box),
+    });
+  }
+
+  removeBox(e, box_id) {
+    this.setState({
+      skill_boxes: this.state.skill_boxes.filter(
+        (box) => box.box_id !== box_id
+      ),
+    });
   }
 
   render() {
+    const { skill_boxes } = this.state;
+    console.log(skill_boxes);
     return (
       <Form.Group className="mb-3" controlId="Skills">
         <h3> Skills</h3>
@@ -28,19 +61,43 @@ class Skills extends Component {
         </Form.Text>
 
         <Accordion defaultActiveKey="0">
-          <Accordion.Item className="mb-3" eventKey="0">
-            <Accordion.Header>(Not specified)</Accordion.Header>
-            <Accordion.Body>
-              <Row className="mb-3">
-                <Form.Label>Skill</Form.Label>
-                <Form.Control className="blue-bg" type="text" />
-              </Row>
-              <Button variant="danger">Delete</Button>
-            </Accordion.Body>
-          </Accordion.Item>
+          {skill_boxes.map((box) => {
+            return (
+              <Accordion.Item
+                className="mb-3"
+                key={box.box_id}
+                eventKey={box.box_id}
+              >
+                <Accordion.Header>
+                  {" "}
+                  {box.skill_name === "" ? "(Not specified)" : box.skill_name}
+                </Accordion.Header>
+
+                <Accordion.Body>
+                  <Row className="mb-3">
+                    <Form.Label>Skill</Form.Label>
+                    <Form.Control
+                      onChange={(e) => this.updateBox(e, box.box_id)}
+                      className="blue-bg"
+                      type="text"
+                    />
+                  </Row>
+
+                  <Button
+                    onClick={(e) => this.removeBox(e, box.box_id)}
+                    variant="danger"
+                  >
+                    Delete
+                  </Button>
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
         </Accordion>
 
-        <Button variant="secondary">+ Add one more skill</Button>
+        <Button onClick={this.addBox} variant="secondary">
+          + Add one more skill
+        </Button>
       </Form.Group>
     );
   }
