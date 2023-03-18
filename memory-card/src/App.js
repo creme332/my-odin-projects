9cd8452e0f9441d1171f1b149323b0927422a55a
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-18 19:02:36 +0400

    memory: add icons and working audio

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,18 +1,62 @@
 import "./App.css";
-import { useState } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import uniqid from "uniqid";
 import imgSrc from "./assets/man-with-cat.gif";
 import imgSrc1 from "./assets/school-woman.png";
+import { GrCircleInformation } from "react-icons/gr";
+import { GiSoundOff, GiSoundOn } from "react-icons/gi";
+import { IconContext } from "react-icons";
+import CorrectSoundUrl from "./assets/correct-6033.mp3";
+import IncorrectSoundUrl from "./assets/error-call-to-attention-129258.mp3";
 
-function SoundButton({ onSoundClick }) {
-  const [soundOn, setSound] = useState(0);
-  // setSound(soundOn);
-  return <button onClick={onSoundClick}>Sound</button>;
+function SoundButton() {
+  const [soundOn, setSound] = useState(false);
+  const [playing, toggle] = useAudio(CorrectSoundUrl);
+  const [playingx, togglec] = useAudio(CorrectSoundUrl);
+
+  function onSoundClick() {
+    console.log("sound button pressed");
+    setSound(!soundOn);
+    toggle();
+  }
+
+  return (
+    <button className="tog-btn" onClick={onSoundClick}>
+      <IconContext.Provider value={{ size: 42 }}>
+        {soundOn ? <GiSoundOff /> : <GiSoundOn />}
+      </IconContext.Provider>
+    </button>
+  );
 }
 
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audio.play() : (audio.currentTime = 0);
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
 function InfoButton({ onInfoClick }) {
-  return <button onClick={onInfoClick}>Info</button>;
+  return (
+    <button className="tog-btn" size="10x" onClick={onInfoClick}>
+      <IconContext.Provider value={{ size: 42 }}>
+        <GrCircleInformation />
+      </IconContext.Provider>
+    </button>
+  );
 }
 
 function CardContainer() {
@@ -44,6 +88,10 @@ function CardContainer() {
     },
   ];
   const imgHeight = 250;
+
+  function shuffle() {
+    return 0;
+  }
   return (
     <div className="card-container">
       {allCards.map((cardDetail) => {
@@ -60,12 +108,10 @@ function CardContainer() {
   );
 }
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   // const allCards = [];
-
-  function onSoundClick() {
-    console.log("Sound button clicked");
-  }
-
   function onInfoClick() {
     console.log("Info button clicked");
   }
@@ -73,7 +119,7 @@ function App() {
   return (
     <div className="App">
       <InfoButton onInfoClick={onInfoClick} />
-      <SoundButton onSoundClick={onSoundClick} />
+      <SoundButton />
       <h1>memory card</h1>
       <CardContainer />
     </div>
