7a85f4a71167b291d5f76f00ce54cc8f81bc95f2
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-20 18:26:34 +0400

    memory: add function to load assets

---
+++ memory-card/src/cards.js
@@ -0,0 +1,109 @@
+import uniqid from "uniqid";
+import img1 from "./assets/images/1.png";
+import img2 from "./assets/images/2.gif";
+import img3 from "./assets/images/3.gif";
+import img4 from "./assets/images/4.gif";
+import img5 from "./assets/images/5.gif";
+import img6 from "./assets/images/6.png";
+import img7 from "./assets/images/7.png";
+import img8 from "./assets/images/8.gif";
+import img9 from "./assets/images/9.png";
+import img10 from "./assets/images/10.png";
+import img11 from "./assets/images/11.gif";
+import img12 from "./assets/images/12.png";
+import img13 from "./assets/images/13.png";
+import img14 from "./assets/images/14.gif";
+import img15 from "./assets/images/15.gif";
+import img16 from "./assets/images/16.png";
+import img17 from "./assets/images/17.gif";
+
+export default function getCards() {
+  const allCards = [
+    {
+      src: img1,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img2,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img3,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img4,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img5,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img6,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img7,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img8,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img9,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img10,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img11,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img12,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img13,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img14,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img15,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img16,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+    {
+      src: img17,
+      alt: "Picture from Janet Mac",
+      id: uniqid(),
+    },
+  ];
+  return allCards;
+}
