d20e8249683aa7399389a6a72ab973be22546566
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-29 15:05:47 +0400

    shop: make navbar dynamic

--- shopping-cart/src/components/NavBar.js
+++ shopping-cart/src/components/NavBar.js
@@ -1,24 +1,43 @@
-import { Link } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
+import NavCSS from "./../styles/NavBar.module.css";
 function NavBar() {
-  const navStyle = {
+  const location = useLocation();
+  const linkStyle = {
     textDecoration: "none",
   };
   return (
-    <nav>
-      <div className="logo">🍌 kram kram banane.</div>
-      <ul>
-        <Link style={navStyle} to="/">
-          <li>Home</li>
+    <nav className={NavCSS.content}>
+      <div className={NavCSS.logo}>🍌 kram kram banane.</div>
+      <ul className={NavCSS.ul}>
+        <Link style={linkStyle} to="/">
+          <li className={location.pathname === "/" ? NavCSS.bold : NavCSS.li}>
+            Home
+          </li>
         </Link>
-        <Link style={navStyle} to="/products">
-          <li>Products</li>
+        <Link style={linkStyle} to="/products">
+          <li
+            className={
+              location.pathname === "/products" ? NavCSS.bold : NavCSS.li
+            }
+          >
+            Products
+          </li>
         </Link>
-        <Link style={navStyle} to="/about">
-          <li>About</li>
+        <Link style={linkStyle} to="/about">
+          <li
+            className={location.pathname === "/about" ? NavCSS.bold : NavCSS.li}
+          >
+            About
+          </li>
         </Link>
-        <Link style={navStyle} to="/contact">
-          <li>Contact</li>
+        <Link style={linkStyle} to="/contact">
+          <li
+            className={
+              location.pathname === "/contact" ? NavCSS.bold : NavCSS.li
+            }
+          >
+            Contact
+          </li>
         </Link>
       </ul>
     </nav>
