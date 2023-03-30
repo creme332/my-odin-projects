bd7872a6486a0f8265f22a0379f834b2f4225737
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-30 11:16:55 +0400

    shop: complete home page

--- shopping-cart/src/components/Home.js
+++ shopping-cart/src/components/Home.js
@@ -1,7 +1,27 @@
 import HomeCSS from "./../styles/Home.module.css";
+import photo from "./../assets/images/bana.jpg";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div className={HomeCSS.home}>Home</div>;
+  const linkStyle = {
+    textDecoration: "none",
+  };
+  return (
+    <div className={HomeCSS.home}>
+      <div className={HomeCSS.main}>
+        <h1>Healthy snacking made delicious🍌</h1>
+        <p>
+          Buy our banana chips made from 100% organic bananas. Choose from a
+          wide variety of flavours including salty, caramel, smoke barbecue and
+          many more.
+        </p>
+        <Link style={linkStyle} to="/products">
+          <button className={HomeCSS.button}>Order</button>
+        </Link>
+      </div>
+      <img src={photo} alt="Banana surrounded by ingredients" />{" "}
+    </div>
+  );
 }
 
 export default Home;
