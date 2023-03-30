6cddac8b0c769f4af1a3ab7111e7bddfaef69693
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-30 12:41:11 +0400

    shop: complete about page

--- shopping-cart/src/components/About.js
+++ shopping-cart/src/components/About.js
@@ -1,7 +1,32 @@
 import AboutCSS from "./../styles/About.module.css";
 
 function About() {
-  return <div className={AboutCSS.about}>About page</div>;
+  return (
+    <div className={AboutCSS.about}>
+      {" "}
+      <div className={AboutCSS.b}></div>
+      <div className={AboutCSS.c}>About us</div>
+      <div className={AboutCSS.d}>
+        <p>
+          At Kram Kram Banane, we believe that food should not only be
+          delicious, but also sustainable and locally-sourced. Our vision is to
+          provide customers with a unique snacking experience that is both
+          satisfying and guilt-free. We source our organic bananas from local
+          farmers, fry them with an airfryer for a healthier snack, and even
+          turn the banana peels into compost for sustainable agriculture.
+        </p>
+        <p>
+          We aim to be a socially responsible company that supports our
+          community and environment. Through our business, we hope to promote
+          sustainable agriculture practices and contribute to the local economy
+          by creating job opportunities. Our ultimate goal is to become a
+          leading producer of high-quality banana chips that prioritize taste,
+          health, and sustainability.
+        </p>
+      </div>
+      <div className={AboutCSS.f}></div>
+    </div>
+  );
 }
 
 export default About;
