94bfa3865e1206909bfd8cc80c8dde7cd5b244fc
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-31 13:40:27 +0400

    shop: add card container to product page

--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -1,8 +1,25 @@
 import ProdCSS from "./../styles/Products.module.css";
-
+import Card from "./Card";
 
 function Products() {
-  return <div className={ProdCSS.products}>Products page</div>;
+  return (
+    <div className={ProdCSS.products}>
+      <h1>Our flavours</h1>
+      <input placeholder="Search..." type="text" />
+      <div className={ProdCSS.cardContainer}>
+        <Card
+          title="Natural"
+          price="15"
+          imgAlt="Natural flavour banana chips"
+        />
+        <Card
+          title="Barbecue"
+          price="20"
+          imgAlt="Natural flavour banana chips"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Products;
