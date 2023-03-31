fa55168f0d81fc768cfd39a8ccb6859d1a39c6b2
creme332 <65414576+creme332@users.noreply.github.com>
2023-03-31 13:39:46 +0400

    shop: complete code for card

--- shopping-cart/src/components/Card.js
+++ shopping-cart/src/components/Card.js
@@ -1,8 +1,21 @@
 import CardCSS from "./../styles/Card.module.css";
+import { Link } from "react-router-dom";
 
-function Card() {
-    return <div className={CardCSS.content} >Card</div>;
-  }
-  
-  export default Card;
-  
+function Card({ title, price, imgSrc, imgAlt }) {
+  const noTextDecorationStyle = { textDecoration: "none" };
+  return (
+    <Link style={{ textDecoration: "none" }} to={`/products/${title}`}>
+      <div className={CardCSS.card}>
+        <img src={imgSrc} alt={imgAlt ? imgAlt : "Image not found"} />
+        <div className={CardCSS.cardDesc}>
+          <span>{title}</span>
+          <span className={`${CardCSS.price} ${noTextDecorationStyle}`}>
+            Rs {parseInt(price)}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default Card;
