579dd99c63299eabb3e8fbd94c72748bc4e16274
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-03 17:02:04 +0400

    shop: add search-icon to search-bar

--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -1,5 +1,8 @@
 import ProdCSS from "./../styles/Products.module.css";
 import Card from "./Card";
+import { BiSearch } from "react-icons/bi";
+import { IconContext } from "react-icons";
+
 import chiliSrc from "./../assets/images/chilli.png";
 import naturalSrc from "./../assets/images/leaf.png";
 import limeSrc from "./../assets/images/lime.png";
@@ -8,7 +11,14 @@ function Products() {
   return (
     <div className={ProdCSS.products}>
       <h1>Our flavours</h1>
-      <input placeholder="Search..." type="text" />
+      <div className={ProdCSS.searchBar}>
+        <div className={ProdCSS.searchIcon}>
+          <IconContext.Provider value={{ size: 25 }}>
+            <BiSearch />
+          </IconContext.Provider>
+        </div>
+        <input maxLength={20} placeholder="Search..." type="text" />
+      </div>
       <div className={ProdCSS.cardContainer}>
         <Card
           title="Natural"
