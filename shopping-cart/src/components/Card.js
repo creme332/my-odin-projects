8d12c8e9971a0f5f2b267d562e22d22df475c178
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-03 21:08:18 +0400

    shop: add hover animation on card

--- shopping-cart/src/components/Card.js
+++ shopping-cart/src/components/Card.js
@@ -1,11 +1,12 @@
 import CardCSS from "./../styles/Card.module.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Card({ title, price, imgSrc, imgAlt }) {
   const noTextDecorationStyle = { textDecoration: "none" };
   return (
     <Link style={{ textDecoration: "none" }} to={`/products/${title}`}>
-      <div className={CardCSS.card}>
+      <motion.div whileHover={{ scale: 1.1 }} className={CardCSS.card}>
         <img src={imgSrc} alt={imgAlt ? imgAlt : "Image not found"} />
         <div className={CardCSS.cardDesc}>
           <span>{title}</span>
@@ -13,7 +14,7 @@ function Card({ title, price, imgSrc, imgAlt }) {
             Rs {parseInt(price)}
           </span>
         </div>
-      </div>
+      </motion.div>
     </Link>
   );
 }
