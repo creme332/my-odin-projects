4fd3a748783785da56525591b0374b7e7a680102
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-03 21:07:19 +0400

    shop: refactor `NavBar.js` and add underline animation

--- shopping-cart/src/components/NavBar.js
+++ shopping-cart/src/components/NavBar.js
@@ -2,48 +2,40 @@ import { Link, useLocation } from "react-router-dom";
 import NavCSS from "./../styles/NavBar.module.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { IconContext } from "react-icons";
+import { motion } from "framer-motion";
 
 function NavBar() {
   const location = useLocation();
   const linkStyle = {
     textDecoration: "none",
   };
+  const tabs = [
+    { tabName: "Home", pathname: "/" },
+    { tabName: "Products", pathname: "/products" },
+    { tabName: "About", pathname: "/about" },
+    { tabName: "Contact", pathname: "/contact" },
+  ];
   return (
     <nav className={NavCSS.content}>
       <Link style={linkStyle} to="/">
         <div className={NavCSS.logo}>🍌 kram kram banane.</div>
       </Link>
       <ul className={NavCSS.ul}>
-        <Link style={linkStyle} to="/">
-          <li className={location.pathname === "/" ? NavCSS.bold : NavCSS.li}>
-            Home
-          </li>
-        </Link>
-        <Link style={linkStyle} to="/products">
-          <li
-            className={
-              location.pathname === "/products" ? NavCSS.bold : NavCSS.li
-            }
-          >
-            Products
-          </li>
-        </Link>
-        <Link style={linkStyle} to="/about">
-          <li
-            className={location.pathname === "/about" ? NavCSS.bold : NavCSS.li}
-          >
-            About
-          </li>
-        </Link>
-        <Link style={linkStyle} to="/contact">
-          <li
-            className={
-              location.pathname === "/contact" ? NavCSS.bold : NavCSS.li
-            }
-          >
-            Contact
-          </li>
-        </Link>
+        {tabs.map((tab) => {
+          return (
+            <Link style={linkStyle} to={tab.pathname}>
+              <li>
+                {tab.tabName}
+                {location.pathname === tab.pathname ? (
+                  <motion.div
+                    className={NavCSS.underline}
+                    layoutId="underline"
+                  />
+                ) : null}
+              </li>
+            </Link>
+          );
+        })}
         <li style={linkStyle} className={NavCSS.shoppingCart}>
           <span className={NavCSS.number}>1</span>
           <IconContext.Provider value={{ size: 30 }}>
