7bc23009a182cd023e0299a66aa01a9a6252a33d
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-10 14:29:53 +0400

    shop: load inventory in shopping cart for testing

--- shopping-cart/src/components/ShoppingCart.js
+++ shopping-cart/src/components/ShoppingCart.js
@@ -1,38 +1,46 @@
 import { Card, Image, Text, Badge, Button, Group, Flex } from "@mantine/core";
+import getInventory from "../cartProvider";
 
-const ShoppingCart = (items) => {
+const ShoppingCart = () => {
+  const items = getInventory();
   return (
     <div>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Card.Section>
-          <Image
-            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-            height={160}
-            alt="Norway"
-          />
-        </Card.Section>
+      {items.map((item) => {
+        return (
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image src={item.imgSrc} height={100} alt={item.imgAlt} />
+            </Card.Section>
 
-        <Group position="apart" mt="md" mb="xs">
-          <Text weight={500}>Norway Fjord Adventures</Text>
-          <Badge color="pink" variant="light">
-            On Sale
-          </Badge>
-        </Group>
+            <Group position="apart" mt="md" mb="xs">
+              <Text weight={500}>Norway Fjord Adventures</Text>
+              <Badge color="pink" variant="light">
+                On Sale
+              </Badge>
+            </Group>
 
-        <Text size="sm" color="dimmed">
-          With Fjord Tours you can explore more of the magical fjord landscapes
-          with tours and activities on and around the fjords of Norway
-        </Text>
-
-        <Flex gap="md">
-          <Button variant="light" color="blue" mt="md" radius="md">
-            +
-          </Button>
-          <Button variant="light" color="blue" mt="md" radius="md">
-            -
-          </Button>
-        </Flex>
-      </Card>
+            <Group position="apart" mt="md">
+              <Flex gap="md">
+                <Button variant="filled" color="blue" mt="md" radius="md">
+                  +
+                </Button>
+                <Text weight={500}>5</Text>
+                <Button variant="filled" color="blue" mt="md" radius="md">
+                  -
+                </Button>
+              </Flex>
+              <Button variant="filled" color="red" mt="md" radius="md">
+                Remove
+              </Button>
+            </Group>
+          </Card>
+        );
+      })}
+      <Flex justify={"center"}>
+        <Button variant="filled" color="orange" mt="md" radius="md">
+          Checkout
+        </Button>
+      </Flex>
     </div>
   );
 };
