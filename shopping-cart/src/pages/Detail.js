8e55870a30b6515356b719b5cedb59b0fb26a338
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-12 21:47:42 +0400

    shop: complete detail page

--- shopping-cart/src/pages/Detail.js
+++ shopping-cart/src/pages/Detail.js
@@ -1,5 +1,8 @@
 import styles from "./../styles/Detail.module.css";
 import { useLocation } from "react-router-dom";
+import { Button, Image, Text, Box } from "@mantine/core";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 function Detail({ clickHandler }) {
   const cardInfo = useLocation().state;
 
@@ -8,7 +11,26 @@ function Detail({ clickHandler }) {
   return (
     <div className={styles.detail}>
       <h1 className="defaultH1">{cardInfo.title}</h1>
-      <p>{cardInfo.description}</p>
+      <Image
+        width={200}
+        height={200}
+        src={cardInfo.imgSrc}
+        alt={cardInfo.imgAlt}
+        withPlaceholder
+      />
+      <Box w={500}>
+        {" "}
+        <Text fz="lg" lineClamp={4}>
+          {cardInfo.description}
+        </Text>
+      </Box>
+      <Button
+        leftIcon={<AiOutlineShoppingCart />}
+        className="defaultButton"
+        loading
+      >
+        Add to Cart
+      </Button>{" "}
     </div>
   );
 }
