3d05d66279b179199caa24a1cda40ef1334ea878
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-15 14:29:55 +0400

    shop: use `getBadge` from respective file

--- shopping-cart/src/components/Card.js
+++ shopping-cart/src/components/Card.js
@@ -1,26 +1,9 @@
 import CardCSS from "./../styles/Card.module.css";
 import { motion } from "framer-motion";
-import { Group, Badge, Image } from "@mantine/core";
+import { Group, Image } from "@mantine/core";
+import getBadge from "../utils/cartBadgeProvider";
+
 function Card({ title, price, imgSrc, imgAlt, status }) {
-  function getBadge(status) {
-    if (status < 0 || status > 2) {
-      return (
-        <Badge color="black" variant="dot">
-          Error
-        </Badge>
-      );
-    }
-    const badge_info = {
-      0: { color: "teal", text: "On Sale" },
-      1: { color: "violet", text: "Limited" },
-      2: { color: "pink", text: "Sold" },
-    };
-    return (
-      <Badge color={badge_info[status].color} variant="light">
-        {badge_info[status].text}
-      </Badge>
-    );
-  }
   const noTextDecorationStyle = { textDecoration: "none" };
   return (
     <motion.div whileHover={{ scale: 1.1 }} className={CardCSS.card}>
