de4cfa1714bedc79f6991ddfbf0e27f0bbceb400
creme332 <65414576+creme332@users.noreply.github.com>
2023-04-28 21:41:19 +0400

    add autocompletion and msg when no prod found

--- shopping-cart/src/pages/Products.js
+++ shopping-cart/src/pages/Products.js
@@ -5,13 +5,51 @@ import { motion } from "framer-motion";
 import getInventory from "../utils/cartProvider";
 import { Link } from "react-router-dom";
 import uniqid from "uniqid";
-import { Input } from '@mantine/core';
 import { useState } from "react";
+import { Autocomplete } from '@mantine/core';
+import { Text } from "@mantine/core";
 
 function Products() {
   const [searchBarValue, setSearchBarValue] = useState("");
   const inventory = getInventory();
 
+  function getMatchedProducts() {
+    const regex = new RegExp(searchBarValue, "i");
+    const foundProductCount = inventory.reduce((acc, card) => acc + (regex.test(card.title) ? 1 : 0), 0);
+    if (foundProductCount === 0) {
+      return (
+        <Text
+          ta="center"
+          sx={{ margin: "auto" }}
+          fz="xl"
+          fw={500} align="center">Flavour not found 😥
+        </Text>
+      );
+    }
+
+    return (inventory.map((card) => {
+      const regex = new RegExp(searchBarValue, "i");
+      if (!regex.test(card.title)) {
+        return null;
+      }
+      return (
+        <Link
+          key={uniqid()}
+          style={{ textDecoration: "none" }}
+          state={card}
+          to={`/products/${card.id}`}
+        >
+          <Card
+            title={card.title}
+            price={card.price}
+            imgAlt={card.imgAlt}
+            imgSrc={card.imgSrc}
+            status={card.status}
+          />
+        </Link>
+      );
+    }))
+  }
   return (
     <motion.div
       className={ProdCSS.products}
@@ -22,39 +60,19 @@ function Products() {
     >
       <h1 className="defaultH1">Our flavours</h1>
 
-      <Input maxLength={20}
-        onChange={(e) => setSearchBarValue(e.target.value)}
-        icon={<BiSearch />
-        }
+      <Autocomplete
+        onChange={(value) => setSearchBarValue(value)}
+        icon={<BiSearch />}
         placeholder="Search..."
         radius="xl"
         size="md"
+        maxDropdownHeight="100"
         aria-label="search bar"
+        data={inventory.map(el => el.title)}
       />
 
       <div className={ProdCSS.cardContainer}>
-        {inventory.map((card) => {
-          const regex = new RegExp(searchBarValue, "i");
-          if (!regex.test(card.title)) {
-            return null;
-          }
-          return (
-            <Link
-              key={uniqid()}
-              style={{ textDecoration: "none" }}
-              state={card}
-              to={`/products/${card.id}`}
-            >
-              <Card
-                title={card.title}
-                price={card.price}
-                imgAlt={card.imgAlt}
-                imgSrc={card.imgSrc}
-                status={card.status}
-              />
-            </Link>
-          );
-        })}
+        {getMatchedProducts()}
       </div>
     </motion.div>
   );
