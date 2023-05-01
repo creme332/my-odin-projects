7f27c6096941d264130a5d885c8bcf891dc069e6
creme332 <65414576+creme332@users.noreply.github.com>
2023-05-01 17:05:07 +0400

    add image carousel

---
+++ photo-tagging/src/components/Card.js
@@ -0,0 +1,55 @@
+import { createStyles, Paper, Text, Title, Button, rem } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    height: rem(440),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 900,
+    color: theme.white,
+    lineHeight: 1.2,
+    fontSize: rem(32),
+    marginTop: theme.spacing.xs,
+  },
+
+  category: {
+    color: theme.white,
+    opacity: 0.7,
+    fontWeight: 700,
+    textTransform: "uppercase",
+  },
+}));
+
+export default function Card({ image, title, category }) {
+  const { classes } = useStyles();
+
+  return (
+    <Paper
+      shadow="md"
+      p="xl"
+      radius="md"
+      sx={{ backgroundImage: `url(${image})` }}
+      className={classes.card}
+    >
+      <div>
+        <Text className={classes.category} size="xs">
+          {category}
+        </Text>
+        <Title order={3} className={classes.title}>
+          {title}
+        </Title>
+      </div>
+      <Button variant="white" color="dark">
+        Play
+      </Button>
+    </Paper>
+  );
+}
---
+++ photo-tagging/src/components/CardsCarousel.js
@@ -0,0 +1,65 @@
+import Card from "./Card";
+import { Carousel } from "@mantine/carousel";
+import { useMediaQuery } from "@mantine/hooks";
+import { useMantineTheme, rem } from "@mantine/core";
+
+export default function CardsCarousel() {
+  const data = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+      title: "Explorer's Realm",
+      category: "nature",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+      title: "Adventure Atlas",
+      category: "beach",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+      title: "Pathfinder's Playground",
+      category: "nature",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+      title: "Navigator's Nirvana",
+      category: "nature",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+      title: "Map Maverick",
+      category: "tourism",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+      title: "Enigma Forever",
+      category: "nature",
+    },
+  ];
+  const theme = useMantineTheme();
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const slides = data.map((item) => (
+    <Carousel.Slide key={item.title}>
+      <Card {...item} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <Carousel
+      withIndicators
+      slideSize="30%"
+      breakpoints={[{ maxWidth: "sm", slideSize: "100%", slideGap: rem(2) }]}
+      slideGap="xl"
+      align="start"
+      slidesToScroll={mobile ? 1 : 1}
+    >
+      {slides}
+    </Carousel>
+  );
+}
