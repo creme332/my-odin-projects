2ccff9900efbf928aa33c1c38ccf3f2a879d6991
creme332 <65414576+creme332@users.noreply.github.com>
2023-05-04 21:03:36 +0400

    use css reset from mantine provider

--- photo-tagging/src/RouteSwitch.js
+++ photo-tagging/src/RouteSwitch.js
@@ -6,29 +6,29 @@ import HeaderWithTabs from "./components/HeaderWithTabs";
 import Leaderboard from "./pages/Leaderboard";
 import Play from "./pages/Play";
 import uniqid from "uniqid";
+import { MantineProvider } from "@mantine/core";
 
 const RouteSwitch = () => {
   const [user, setUser] = useState({ name: "John", image: leafLogo });
   const tabs = [
     { tabName: "Home", pathname: "/", id: uniqid() },
-    { tabName: "Play", pathname: "/play", id: uniqid() },
     { tabName: "Leaderboard", pathname: "/leaderboard", id: uniqid() },
   ];
   return (
-    <BrowserRouter basename="/my-odin-projects/photo-tagging/build">
-      <HeaderWithTabs
-        links={[
-          { label: "Home", link: "/" },
-          { label: "Play", link: "/play" },
-          { label: "Leaderboard", link: "/leaderboard" },
-        ]}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/play" element={<Play />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-      </Routes>
-    </BrowserRouter>
+    <MantineProvider
+      theme={{ colorScheme: "light" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <BrowserRouter basename="/my-odin-projects/photo-tagging/build">
+        <HeaderWithTabs links={tabs} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play/:id" element={<Play />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 };
 
--- photo-tagging/src/index.js
+++ photo-tagging/src/index.js
@@ -1,4 +1,3 @@
-import "./styles/reset.css";
 import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
