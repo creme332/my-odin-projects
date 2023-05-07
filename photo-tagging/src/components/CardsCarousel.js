9ff8bc09424911c8dd48a33a9cb058ce4a8447a7
creme332 <65414576+creme332@users.noreply.github.com>
2023-05-07 10:37:06 +0400

    remove `data` from carousel

--- photo-tagging/src/components/CardsCarousel.js
+++ photo-tagging/src/components/CardsCarousel.js
@@ -3,51 +3,7 @@ import { Carousel } from "@mantine/carousel";
 import { useMediaQuery } from "@mantine/hooks";
 import { useMantineTheme, rem } from "@mantine/core";
 
-export default function CardsCarousel() {
-  const data = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-      title: "Explorer's Realm",
-      category: "nature",
-      available: true,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-      title: "Adventure Atlas",
-      category: "beach",
-      available: true,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-      title: "Pathfinder's Playground",
-      category: "nature",
-      available: true,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-      title: "Navigator's Nirvana",
-      category: "nature",
-      available: false,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-      title: "Map Maverick",
-      category: "tourism",
-      available: false,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-      title: "Enigma Forever",
-      category: "nature",
-      available: false,
-    },
-  ];
+export default function CardsCarousel({data}) {
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
 
