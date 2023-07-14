6c1eb3b33f6a098a04e345dd06ce26054cfc447e
creme332 <65414576+creme332@users.noreply.github.com>
2023-07-14 17:45:14 +0400

    create a separate file for rebalance

---
+++ js-final/src/utils/rebalance.js
@@ -0,0 +1,35 @@
+import { differenceInDays, addDays, format, sub } from "date-fns";
+
+/**
+ * Automatically fills entries for days where user input is missing.
+ * Dates between start date and current date, where current date >= start date, are filled.
+ * @param {String} startDate Date when habit started
+ * @param {[{date:String, value:integer}]} entryList list of all entries
+ * @param {integer} defaultValue
+ * @returns {[{date:String, value:integer}]}
+ */
+export default function rebalanceEntries(startDate, entryList, defaultValue) {
+  console.log(startDate, entryList, defaultValue);
+  const allDateEntries = entryList.map((e) => e.date);
+  const daysSinceCreation = differenceInDays(new Date(), new Date(startDate)); // number of days since habit creation
+  const newEntryList = [...entryList];
+
+  // fill gaps between entries with default value up until today
+  if (daysSinceCreation > 0) {
+    for (let day = 0; day <= daysSinceCreation; day++) {
+      const date = format(addDays(new Date(startDate), day), "yyyy-MM-dd");
+      if (!allDateEntries.includes(date)) {
+        newEntryList.push({ date: date, value: defaultValue });
+      }
+    }
+  }
+
+  // initialise entry list for last 7 days including today
+  for (let day = 6; day >= 0; day--) {
+    const date = format(sub(new Date(), { days: day }), "yyyy-MM-dd");
+    if (!allDateEntries.includes(date)) {
+      newEntryList.push({ date: date, value: defaultValue });
+    }
+  }
+  return newEntryList;
+}
