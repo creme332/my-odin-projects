0065dbb37d3053f40fea5f82d54f963df81f89a4
creme332 <65414576+creme332@users.noreply.github.com>
2023-08-14 15:40:18 +0400

    create stats page

--- js-final/src/pages/habits/[id].js
+++ js-final/src/pages/habits/[id].js
@@ -1,6 +1,36 @@
 import { useRouter } from "next/router";
+import { Container, Button, Title } from "@mantine/core";
+import StatsGrid from "@/components/stats/Grid";
+import LineChart from "@/components/stats/LineChart";
 
-export default function Page() {
+export default function HabitDetailPage({ deleteHabit }) {
+  console.log(deleteHabit);
+  const { query } = useRouter();
+  const habit = JSON.parse(query.habit);
   const router = useRouter();
-  return <p>Habit ID: {router.query.id}</p>;
+
+  return (
+    <Container>
+      <Title order={1}>{habit.name}</Title>
+      <StatsGrid />
+      <Title order={2}>Score</Title>
+      <LineChart
+        color={habit.color}
+        dataArray={[100, 200, 200]}
+        labelsArray={["01/02", "01/03", "01/05"]}
+      />
+
+      <Button
+        onClick={() => {
+          deleteHabit(habit.id);
+          router.push({
+            pathname: "/dashboard",
+          });
+        }}
+        color="red"
+      >
+        Delete habit
+      </Button>
+    </Container>
+  );
 }
