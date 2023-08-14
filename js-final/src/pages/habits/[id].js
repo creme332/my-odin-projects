e805820a8161bf51a7226553d9d247c20fea2c1d
creme332 <65414576+creme332@users.noreply.github.com>
2023-08-14 16:28:09 +0400

    add export data option

--- js-final/src/pages/habits/[id].js
+++ js-final/src/pages/habits/[id].js
@@ -1,11 +1,13 @@
 import { useRouter } from "next/router";
-import { Container, Button, Title } from "@mantine/core";
+import { Container, Group, Button, Title, Code } from "@mantine/core";
 import StatsGrid from "@/components/stats/Grid";
 import LineChart from "@/components/stats/LineChart";
+import { useState } from "react";
 
 export default function HabitDetailPage({ deleteHabit }) {
-  console.log(deleteHabit);
+  // console.log(deleteHabit);
   const { query } = useRouter();
+  const [showHabitData, setShowHabitData] = useState(false);
   const habit = JSON.parse(query.habit);
   const router = useRouter();
 
@@ -20,17 +22,32 @@ export default function HabitDetailPage({ deleteHabit }) {
         labelsArray={["01/02", "01/03", "01/05"]}
       />
 
-      <Button
-        onClick={() => {
-          deleteHabit(habit.id);
-          router.push({
-            pathname: "/dashboard",
-          });
-        }}
-        color="red"
-      >
-        Delete habit
-      </Button>
+      <Group>
+        <Button
+          mt={20}
+          onClick={() => {
+            setShowHabitData(!showHabitData);
+          }}
+          color="orange"
+        >
+          Export data
+        </Button>
+        <Button
+          mt={20}
+          onClick={() => {
+            deleteHabit(habit.id);
+            router.push({
+              pathname: "/dashboard",
+            });
+          }}
+          color="red"
+        >
+          Delete habit
+        </Button>
+      </Group>
+      {showHabitData ? (
+        <Code block>{JSON.stringify(habit, null, 2)}</Code>
+      ) : null}
     </Container>
   );
 }
