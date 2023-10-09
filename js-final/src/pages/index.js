b9032e53bebabfeb8410800056d59796cc7ce984
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-09 18:28:57 +0400

    refactor

--- js-final/src/pages/index.js
+++ js-final/src/pages/index.js
@@ -1,5 +1,5 @@
 import { Text } from "@mantine/core";
 
-export default function Home({ loggedIn, validateLogin }) {
-  return <Text>qkwiqq is a minimalist habit tracker app</Text>;
+export default function Home({}) {
+  return <Text>qkwiqq is a minimalist habit tracker app.</Text>;
 }
