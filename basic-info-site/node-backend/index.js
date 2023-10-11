ee23823146ee61ec4f5a38987d2694544a0faa50
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-11 10:52:25 +0400

    create a node-backend folder

--- basic-info-site/node-backend/index.js
+++ basic-info-site/node-backend/index.js
@@ -8,20 +8,25 @@ const server = http.createServer(function (req, res) {
   console.log("Requested url: ", req.url);
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    html = fs.readFileSync("./index.html");
+    html = fs.readFileSync("./pages/index.html");
     res.end(html);
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    html = fs.readFileSync("./about.html");
+    html = fs.readFileSync("./pages/about.html");
     res.end(html);
   } else if (req.url === "/contact") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    html = fs.readFileSync("./contact.html");
+    html = fs.readFileSync("./pages/contact.html");
     res.end(html);
+  } else if (req.url === "/style.css") {
+    // serve css file
+    res.writeHead(200, { "Content-Type": "text/css" });
+    css = fs.readFileSync("./styles/style.css");
+    res.end(css);
   } else {
     // page not found
     res.writeHead(200, { "Content-Type": "text/html" });
-    html = fs.readFileSync("./404.html");
+    html = fs.readFileSync("./pages/404.html");
     res.end(html);
   }
 });
