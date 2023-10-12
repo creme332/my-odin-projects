151281367408263a41bb189a1158cbee69da94ae
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-12 15:27:55 +0400

    setup working prototype

---
+++ mini-message-board/routes/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+const router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  console.log("fdsalkf");
+  res.redirect("/");
+});
+
+module.exports = router;
---
+++ mini-message-board/routes/new.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET form listing. */
+router.get("/", function (req, res, next) {
+  res.render("form", {});
+});
+
+module.exports = router;
